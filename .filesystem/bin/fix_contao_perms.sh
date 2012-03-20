8a069055b823f97bd4ed6ac2cc369d2d00495519
Wael Nasreddine <wael.nasreddine@gmail.com>
2012-03-20 23:19:46 +0100

    Add more permissions fixes

--- .filesystem/bin/fix_contao_perms.sh
+++ .filesystem/bin/fix_contao_perms.sh
@@ -5,3 +5,5 @@ mkdir -p public/system/logs
 chmod 777 public/system/{html,logs,scripts,tmp}
 touch public/sitemap.xml
 chmod 666 public/sitemap.xml
+find public/system/modules/efg -type f -exec chmod 666 {} ';'
+find public/system/modules/efg -type d -exec chmod 777 {} ';'
