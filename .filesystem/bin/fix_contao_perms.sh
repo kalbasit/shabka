f89f1d466d30ffbb6a7fc537f6dfbeb0a2d6b725
Wael Nasreddine <wael.nasreddine@gmail.com>
2012-05-20 11:50:13 +0200

    Add some more folders to make world-writable and copy over the default .htaccess

--- .filesystem/bin/fix_contao_perms.sh
+++ .filesystem/bin/fix_contao_perms.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+set -ex
+
 cd $(git rev-parse --show-toplevel)
+#rm -f public/system/config/localconfig.php
+#ln -s ~/Code/Agence_Durable/localconfig.php public/system/config/localconfig.php
+cp public/.htaccess.default public/.htaccess
 mkdir -p public/system/logs
-chmod 777 public/system/{html,logs,scripts,tmp}
+chmod 777 public/system/{html,logs,scripts,tmp,cache} 2>/dev/null
 touch public/sitemap.xml
 chmod 666 public/sitemap.xml
 find public/system/modules/efg -type f -exec chmod 666 {} ';'
