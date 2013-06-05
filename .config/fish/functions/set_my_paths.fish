e64424fec42f05d9acaed84669fa3551168954c3
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2013-06-05 00:29:59 -0700

    Look in opt/ only if its there

--- .config/fish/functions/set_my_paths.fish
+++ .config/fish/functions/set_my_paths.fish
@@ -1,6 +1,4 @@
 function set_my_paths
-  set -l opt_paths (find "$HOME/.filesystem/opt/" -maxdepth 2 -name bin -type d)
-
   # Add my bin folder
   if test -d "$HOME/.filesystem/bin"
     add_path "$HOME/.filesystem/bin"
@@ -12,9 +10,13 @@ function set_my_paths
   end
 
   # Add any folder in my opt folders
-  if count $opt_paths > 0
-    for dir in $opt_paths
-      add_path "$dir"
+  if test -d "$HOME/.filesystem/opt/"
+    set -l opt_paths (find "$HOME/.filesystem/opt/" -maxdepth 2 -name bin -type d)
+
+    if count $opt_paths > 0
+      for dir in $opt_paths
+        add_path "$dir"
+      end
     end
   end
 end
