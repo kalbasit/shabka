9c2d7bd78696fc1472066c0740dc9f8dc1664cf2
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2014-05-21 09:57:56 -0700

    rename MY_FS to MYFS and use chrome-beta only if it's available

--- .oh-my-zsh-ext/my-exports/my-exports.plugin.zsh
+++ .oh-my-zsh-ext/my-exports/my-exports.plugin.zsh
@@ -1,8 +1,13 @@
 export VMAIL_HTML_PART_READER='elinks -dump'
 export GOPATH=$HOME/code/go
 export EDITOR=vim
-export MY_FS=$HOME/.filesystem
-export BROWSER=/usr/bin/google-chrome-beta
+export MYFS=$HOME/.filesystem
+
+if [[ -x /usr/bin/google-chrome-beta ]]; then
+  export BROWSER=/usr/bin/google-chrome-beta
+else
+  export BROWSER=/usr/bin/google-chrome
+fi
 
 if [[ -x `which cask 2> /dev/null` ]]; then
   export CASK_PATH="$(dirname $(dirname $(which cask)))"
