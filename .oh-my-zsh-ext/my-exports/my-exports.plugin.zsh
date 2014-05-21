4bef0013118a51b0e29f82cab92f9962b1744d96
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2014-05-21 09:45:45 -0700

    zsh: export CASK_PATH if cask is installed

--- .oh-my-zsh-ext/my-exports/my-exports.plugin.zsh
+++ .oh-my-zsh-ext/my-exports/my-exports.plugin.zsh
@@ -3,3 +3,7 @@ export GOPATH=$HOME/code/go
 export EDITOR=vim
 export MY_FS=$HOME/.filesystem
 export BROWSER=/usr/bin/google-chrome-beta
+
+if [[ -x `which cask 2> /dev/null` ]]; then
+  export CASK_PATH="$(dirname $(dirname $(which cask)))"
+fi
