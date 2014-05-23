956025dfd502e408c74df73bf3bd004bbe393939
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2014-05-23 00:30:27 -0700

    emacs: reformat

--- .emacs.d/packages.el
+++ .emacs.d/packages.el
@@ -1,3 +1,107 @@
+(use-package notmuch
+  :commands 'notmuch
+  :bind ("C-c m" . notmuch)
+  :init
+  (progn
+    (use-package gnus-alias
+      :config
+      (progn
+        (setq gnus-alias-identity-alist
+        '(("personal"
+           nil ;; Does not refer to any other identity
+           "Wael Nasreddine <wael.nasreddine@gmail.com>" ;; Sender address
+           nil ;; No organization header
+           nil ;; No extra headers
+           nil ;; No extra body text
+           "~/.signatures/personal")
+          ("work"
+           nil
+           "Wael Nasreddine <wmn@google.com>" ;; Sender address
+           "Google"
+           nil
+           nil
+           "~/.signatures/work")))
+        ;; Define the rules TODO: Add all of personal addresses
+        (setq gnus-alias-identity-rules (quote
+                                          (("personal" ("any" "wael.nasreddine@gmail.com" both) "personal")
+                                           ("work" ("any" "\\(wmn\\|wnasreddine\\)@google.com" both) "work"))))
+        ;; Use "work" identity by default
+        (setq gnus-alias-default-identity "work")
+        ;; Determine identity when message-mode loads
+        (add-hook 'message-setup-hook 'gnus-alias-determine-identity))))
+  :config
+  (progn
+    (setq notmuch-saved-searches
+      '((:name "inbox-work-new" :query "tag:work AND tag:unread AND tag:inbox")
+        (:name "inbox-personal-new" :query "tag:personal AND tag:unread AND tag:inbox")
+        (:name "work-new" :query "tag:work AND tag:unread")
+        (:name "personal-new" :query "tag:personal AND tag:unread")
+        (:name "work" :query "tag:work")
+        (:name "personal" :query "tag:personal")
+        (:name "inbox-unread" :query "tag:inbox AND tag:unread")
+        (:name "unread" :query "tag:unread")
+        (:name "inbox" :query "tag:inbox")))
+    (setq notmuch-address-command
+      (f-expand "nottoomuch-addresses" (concat (f-full (getenv "HOME")) "/.filesystem/bin")))
+    (notmuch-address-message-insinuate)
+    (setq mail-envelope-from (quote header))
+    (setq mail-specify-envelope-from t)
+    (setq message-kill-buffer-on-exit t)
+    (setq message-sendmail-envelope-from (quote header))
+    (setq notmuch-fcc-dirs nil)
+    (setq notmuch-search-oldest-first nil)
+    (setq send-mail-function (quote sendmail-send-it))
+    (setq sendmail-program "/usr/bin/msmtp")
+    ;; reply to all by default
+    (define-key notmuch-show-mode-map "r" 'notmuch-show-reply)
+    (define-key notmuch-show-mode-map "R" 'notmuch-show-reply-sender)
+    (define-key notmuch-search-mode-map "r" 'notmuch-search-reply-to-thread)
+    (define-key notmuch-search-mode-map "R" 'notmuch-search-reply-to-thread-sender)
+    ;; Toggle message deletion
+    (define-key notmuch-show-mode-map "d"
+      (lambda ()
+        "toggle deleted tag for message"
+        (interactive)
+        (if (member "deleted" (notmuch-show-get-tags))
+            (notmuch-show-tag (list "-deleted"))
+          (notmuch-show-tag (list "+deleted")))))
+    ;; Mark as a Spam
+    (define-key notmuch-show-mode-map "!"
+      (lambda ()
+        "toggle spam tag for message"
+        (interactive)
+        (if (member "spam" (notmuch-show-get-tags))
+            (notmuch-show-tag (list "-spam"))
+          (notmuch-show-tag (list "+spam")))))
+    ;; Kill a thread
+    (define-key notmuch-show-mode-map "&"
+      (lambda ()
+        "toggle killed tag for message"
+        (interactive)
+        (if (member "killed" (notmuch-show-get-tags))
+            (notmuch-show-tag (list "-killed"))
+          (notmuch-show-tag (list "+killed")))))
+    ;; Bounce a message
+    (define-key notmuch-show-mode-map "b"
+      (lambda (&optional address)
+        "Bounce the current message."
+        (interactive "sBounce To: ")
+        (notmuch-show-view-raw-message)
+        (message-resend address)))
+          (add-hook 'message-setup-hook 'mml-secure-sign-pgpmime)
+    ;; Sign messages by default.
+    (add-hook 'message-setup-hook 'mml-secure-sign-pgpmime)
+    ;; At startup position the cursor on the first saved searches
+    (add-hook 'notmuch-hello-refresh-hook
+        (lambda ()
+          (if (and (eq (point) (point-min))
+             (search-forward "Saved searches:" nil t))
+              (progn
+                (forward-line)
+                (widget-forward 1))
+            (if (eq (widget-type (widget-at)) 'editable-field)
+                (beginning-of-line)))))))
+
 (use-package hl-line
   :config (set-face-background 'hl-line "#073642"))
 
@@ -64,108 +168,6 @@
 
 (use-package git-gutter
   :init (global-git-gutter-mode +1))
-
-(use-package notmuch
-  :commands 'notmuch
-  :bind ("C-c m" . notmuch)
-  :config
-  (progn
-    (use-package gnus-alias
-      :config
-      (progn
-        (setq gnus-alias-identity-alist
-        '(("personal"
-           nil ;; Does not refer to any other identity
-           "Wael Nasreddine <wael.nasreddine@gmail.com>" ;; Sender address
-           nil ;; No organization header
-           nil ;; No extra headers
-           nil ;; No extra body text
-           "~/.signatures/personal")
-          ("work"
-           nil
-           "Wael Nasreddine <wmn@google.com>" ;; Sender address
-           "Google"
-           nil
-           nil
-           "~/.signatures/work")))
-        ;; Define the rules TODO: Add all of personal addresses
-        (setq gnus-alias-identity-rules (quote
-                                          (("personal" ("any" "wael.nasreddine@gmail.com" both) "personal")
-                                           ("work" ("any" "\\(wmn\\|wnasreddine\\)@google.com" both) "work"))))
-        ;; Use "work" identity by default
-        (setq gnus-alias-default-identity "work")
-        ;; Determine identity when message-mode loads
-        (add-hook 'message-setup-hook 'gnus-alias-determine-identity)))
-    (setq notmuch-saved-searches
-    '((:name "inbox-work-new" :query "tag:work AND tag:unread AND tag:inbox")
-      (:name "inbox-personal-new" :query "tag:personal AND tag:unread AND tag:inbox")
-      (:name "work-new" :query "tag:work AND tag:unread")
-      (:name "personal-new" :query "tag:personal AND tag:unread")
-      (:name "work" :query "tag:work")
-      (:name "personal" :query "tag:personal")
-      (:name "inbox-unread" :query "tag:inbox AND tag:unread")
-      (:name "unread" :query "tag:unread")
-      (:name "inbox" :query "tag:inbox")))
-    (setq notmuch-address-command
-      (f-expand "nottoomuch-addresses" (concat (f-full (getenv "HOME")) "/.filesystem/bin")))
-    (notmuch-address-message-insinuate)
-    (setq mail-envelope-from (quote header))
-    (setq mail-specify-envelope-from t)
-    (setq message-kill-buffer-on-exit t)
-    (setq message-sendmail-envelope-from (quote header))
-    (setq notmuch-fcc-dirs nil)
-    (setq notmuch-search-oldest-first nil)
-    (setq send-mail-function (quote sendmail-send-it))
-    (setq sendmail-program "/usr/bin/msmtp")
-    ;; reply to all by default
-    (define-key notmuch-show-mode-map "r" 'notmuch-show-reply)
-    (define-key notmuch-show-mode-map "R" 'notmuch-show-reply-sender)
-    (define-key notmuch-search-mode-map "r" 'notmuch-search-reply-to-thread)
-    (define-key notmuch-search-mode-map "R" 'notmuch-search-reply-to-thread-sender)
-    ;; Toggle message deletion
-    (define-key notmuch-show-mode-map "d"
-      (lambda ()
-        "toggle deleted tag for message"
-        (interactive)
-        (if (member "deleted" (notmuch-show-get-tags))
-            (notmuch-show-tag (list "-deleted"))
-          (notmuch-show-tag (list "+deleted")))))
-    ;; Mark as a Spam
-    (define-key notmuch-show-mode-map "!"
-      (lambda ()
-        "toggle spam tag for message"
-        (interactive)
-        (if (member "spam" (notmuch-show-get-tags))
-            (notmuch-show-tag (list "-spam"))
-          (notmuch-show-tag (list "+spam")))))
-    ;; Kill a thread
-    (define-key notmuch-show-mode-map "&"
-      (lambda ()
-        "toggle killed tag for message"
-        (interactive)
-        (if (member "killed" (notmuch-show-get-tags))
-            (notmuch-show-tag (list "-killed"))
-          (notmuch-show-tag (list "+killed")))))
-    ;; Bounce a message
-    (define-key notmuch-show-mode-map "b"
-      (lambda (&optional address)
-        "Bounce the current message."
-        (interactive "sBounce To: ")
-        (notmuch-show-view-raw-message)
-        (message-resend address)))
-          (add-hook 'message-setup-hook 'mml-secure-sign-pgpmime)
-    ;; Sign messages by default.
-    (add-hook 'message-setup-hook 'mml-secure-sign-pgpmime)
-    ;; At startup position the cursor on the first saved searches
-    (add-hook 'notmuch-hello-refresh-hook
-        (lambda ()
-          (if (and (eq (point) (point-min))
-             (search-forward "Saved searches:" nil t))
-              (progn
-                (forward-line)
-                (widget-forward 1))
-            (if (eq (widget-type (widget-at)) 'editable-field)
-                (beginning-of-line)))))))
 
 (use-package ack-and-a-half
   :commands (ack-and-a-half ack-and-a-half-same ack-and-a-half-find-file ack-and-a-half-find-file-same)
@@ -236,11 +238,11 @@
     (use-package rspec-mode
       :config
       (progn
-  (setq rspec-use-rake-flag nil)
-  (defadvice rspec-compile (around rspec-compile-around activate)
-    "Use BASH shell for running the specs because of ZSH issues."
-    (let ((shell-file-name "/bin/bash"))
-      ad-do-it)))))
+        (setq rspec-use-rake-flag nil)
+        (defadvice rspec-compile (around rspec-compile-around activate)
+          "Use BASH shell for running the specs because of ZSH issues."
+          (let ((shell-file-name "/bin/bash"))
+            ad-do-it)))))
   :config
   (progn
     (setq ruby-deep-indent-paren nil))
@@ -268,11 +270,12 @@
   :bind ("C-c C-e" . erc-start-or-switch)
   :config
   (progn
-    (setq erc-modules (quote
-                        (autoaway autojoin button completion fill irccontrols
-                         list match menu move-to-prompt netsplit networks
-                         noncommands notify readonly ring scrolltobottom smiley
-                         stamp spelling track truncate unmorse)))
+    (setq erc-modules
+          (quote
+            (autoaway autojoin button completion fill irccontrols
+             list match menu move-to-prompt netsplit networks
+             noncommands notify readonly ring scrolltobottom smiley
+             stamp spelling track truncate unmorse)))
     (load "~/.ercpass") ;; load erc passwords
     (setq erc-autojoin-mode t)
     (setq erc-autojoin-channels-alist
