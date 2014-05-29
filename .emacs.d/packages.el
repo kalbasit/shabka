10810255dcc3f716532b0badd6162a73c02cd71d
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2014-05-29 11:10:37 -0700

    emacs: Add more saved searches

--- .emacs.d/packages.el
+++ .emacs.d/packages.el
@@ -33,7 +33,8 @@
   :config
   (progn
     (setq notmuch-saved-searches
-      '((:name "family-new" :query "tag:family AND tag:unread")
+      '((:name "tome-work" :query "tag:unread AND tag:work AND to:wmn@google.com")
+        (:name "family-new" :query "tag:family AND tag:unread")
         (:name "wife-new" :query "tag:wife AND tag:unread")
         (:name "inbox-work-new" :query "tag:work AND tag:unread AND tag:inbox")
         (:name "inbox-personal-new" :query "tag:personal AND tag:unread AND tag:inbox")
