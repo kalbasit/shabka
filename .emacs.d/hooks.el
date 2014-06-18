617da8eb1f790172787280f811f7afce41e7ab4e
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2014-06-18 14:23:36 -0700

    emacs: move the python hook to the hooks file

--- .emacs.d/hooks.el
+++ .emacs.d/hooks.el
@@ -1,2 +1,7 @@
+;; Python indent with two spaces
+(add-hook 'python-mode-hook
+          (function (lambda ()
+                      (setq tab-width 2))))
+
 ;; Enable flyspell for text mode
 (add-hook 'text-mode-hook 'enable-flyspell)
