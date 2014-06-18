2b43eb4f3d4d523772fc68090da12d83fbd5030d
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2014-06-18 13:50:55 -0700

    emacs: add company (auto-complete tooltip)

--- .emacs.d/packages.el
+++ .emacs.d/packages.el
@@ -1,3 +1,15 @@
+(use-package company
+  :init (global-company-mode 1)
+  :config
+  (progn
+    (setq company-idle-delay 0.5)
+    (setq company-tooltip-limit 10)
+    (setq company-minimum-prefix-length 2)
+
+    ;; invert the navigation direction if the the completion popup-isearch-match
+    ;; is displayed on top (happens near the bottom of windows)
+    (setq company-tooltip-flip-when-above t)))
+
 (use-package notmuch
   :commands 'notmuch
   :bind ("C-c m" . notmuch)
