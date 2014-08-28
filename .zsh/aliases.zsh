8ac3cd4229d3938f58a2d8cc3ef6f03cacaff177
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2014-08-28 11:23:50 -0700

    Cleanup the alias file

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,31 +1,13 @@
-# Attach or Create
-alias ac="tmx main"
-
-# ack
-if [[ -x "`which ack-grep 2> /dev/null`" ]]; then
-  alias ack="ack-grep -il"
-fi
-
-# PW
+[[ -x "`which ack-grep 2> /dev/null`" ]] && alias ack="ack-grep -il"
 alias pw="ps aux | grep -v grep | grep -e"
-
-# Serve this
 alias serve_this="python -m SimpleHTTPServer"
 alias rserve_this="ruby -rrack -e \"Rack::Handler::WEBrick.run Rack::Directory.new('.')\""
-
-# XVFB
 alias run_xvfb="Xvfb :4 -screen 0 1280x1024x24"
 alias xr="xvfb-run --server-args='-screen 0 1280x1024x24'"
 alias xrake="xr bundle exec rake"
 alias xrspec="xr bundle exec rspec"
 alias xspec="xrake parallel:prepare parallel:spec"
-
-# Editor
 alias e="subl"
-
 alias email="emacsclient -a '' -s mail -t"
-
 alias history='fc -fl 1'
-
 alias xcopy='xclip -selection clipboard'
-
