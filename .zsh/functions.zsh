1aee79aff92359d5234e22f2d9d760954cbb2153
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2014-09-02 18:35:28 -0700

    Add zssh()

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -554,3 +554,43 @@ function scs() {
 
   eval "${screen_cmd}"
 }
+# zssh()#{{{
+function zssh() {
+    local user_host_port=${1}
+    local SSH=/usr/bin/ssh
+    local REMOTE_SSH_USER=""
+    local DESTZDOTDIR=""
+    local controlmaster_running=""
+
+    # fail-fast if no user/host/port were given
+    if [[ -z "${user_host_port}" ]]; then
+        print_error 0 "Usage zssh <[user@]host[:port]>"
+        return 1
+    fi
+
+    # let's get the user ssh'ing into the host
+    REMOTE_SSH_USER="$(echo $user_host_port | grep @ | cut -d@ -f1)"
+    REMOTE_SSH_USER="${REMOTE_SSH_USER:-$USER}"
+
+    # The private home folder
+    DESTZDOTDIR="/tmp/${USER}.zdotdir.${REMOTE_SSH_USER}"
+
+    if [[ "$($SSH -O check ${user_host_port} &>/dev/null; echo $?)" -eq "0" ]]; then
+        controlmaster_running="true"
+    else
+        controlmaster_running="false"
+    fi
+
+    if isFalse ${controlmaster_running}; then
+        # start the controlmaster
+        $SSH ${user_host_port} /bin/true
+        # Transfer what we need to the server
+        rm -rf $DESTZDOTDIR && mkdir $DESTZDOTDIR
+        cp -RL ${HOME}/.{zsh,zshrc,zshrc-google} $DESTZDOTDIR/
+        scp -r $DESTZDOTDIR/ ${user_host_port}:$DESTZDOTDIR/ >/dev/null
+        infocmp | $SSH -t ${user_host_port} "cat > '${DESTZDOTDIR}/${TERM}.info' && tic '${DESTZDOTDIR}/${TERM}.info'"
+    fi
+
+    $SSH -tt ${user_host_port} ZDOTDIR=${DESTZDOTDIR} zsh -i
+}
+#}}}
