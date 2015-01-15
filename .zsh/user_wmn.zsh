29719d89eded8cf9f51f60cbd3d04340ee00a89a
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-01-15 15:25:39 -0800

    ls --color=never does not work on Darwin, use find instead

--- .zsh/user_wmn.zsh
+++ .zsh/user_wmn.zsh
@@ -31,7 +31,8 @@ if [[ -d "${HOME}/.filesystem" ]]; then
       pathmunge "${MYFS}/opt/go_appengine"
     fi
 
-    for dir in `ls --color=never "${MYFS}/opt"`; do
+    for dir in `find "${MYFS}/opt" -type d -maxdepth 1 -mindepth 1`; do
+      dir="${dir##./}"
       if [[ -d "${MYFS}/opt/${dir}/bin" ]]; then
         pathmunge "${MYFS}/opt/${dir}/bin"
       fi
