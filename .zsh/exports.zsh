6deb60cc612b8d0547b6cae20a0187c98ba3c35a
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-03-09 16:47:41 -0700

    Add github token

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,6 +1,11 @@
 export BROWSER=/usr/bin/google-chrome-unstable
 
 local mysql_credentials_path="$HOME/.my.cnf"
+local github_token_path="$HOME/.github_token"
+
+if [[ -r "${github_token_path}" ]]; then
+  export HOMEBREW_GITHUB_API_TOKEN=`head -1 ${github_token_path}`
+fi
 
 if [[ -r "${mysql_credentials_path}" ]]; then
   local user="`cat "$mysql_credentials_path" | grep "user" | cut -d= -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`"
