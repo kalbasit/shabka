bc58aa36ecd3ea73795549b19f2c16d61948f892
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-03-24 15:02:09 -0700

    Make DM_PROJECT optional and easy to override automatic matching

--- .zsh/git-extensions.zsh
+++ .zsh/git-extensions.zsh
@@ -7,26 +7,26 @@ function gcim() {
     return 1
   fi
 
-  if echo "${PWD}" | grep -q "ads/dmx"; then
-    project="DMX"
+  if echo "${PWD}" | grep -q "ads/dmx\|dailymotion/go-liverail"; then
+    project="[DMX] "
   fi
 
-  if [[ -z "${project}" ]]; then
-    if [[ -z "${DM_PROJECT}" ]]; then
-      echo "Missing project, please export the project name as DM_PROJECT"
-      return 1
-    fi
+  if [[ -z "${DM_PROJECT}" -a  -z "${project}" ]]; then
+    echo "Missing project, please export the project name as DM_PROJECT and amend the commit if needed"
+    project=""
+  fi
 
-    project="${DM_PROJECT}"
+  if [[ -n "${DM_PROJECT}" ]]; then
+    project="[${DM_PROJECT}] "
   fi
 
   if [[ "x`git rev-parse --git-dir 2> /dev/null`" != "x" ]]; then
     local branch="`current_branch`"
     local story="`echo "${branch}" | grep '^\(DAILY\|DMX\)-[[:digit:]][[:digit:]]*$'`"
     if [[ "x${story}" != "x" ]]; then
-      git commit -m "[${project}] ${message} (${story})"
+      git commit -m "${project}${message} (${story})"
     else
-      git commit -m "${message}"
+      git commit -m "${project}${message}"
     fi
   else
     echo "You must be under a git repository to use gcim"
