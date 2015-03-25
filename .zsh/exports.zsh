346fdd0f806fe11374cb99cd2f93413431bfc32f
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-03-25 07:00:37 -0700

    remove local as in bash it works only in functions

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -74,19 +74,22 @@ if [[ -f "${HOME}/.travis/travis.sh" ]]; then
 fi
 
 # Export Github's token if it's readable.
-local github_token_path="$HOME/.github_token"
+github_token_path="$HOME/.github_token"
 if [[ -r "${github_token_path}" ]]; then
   export HOMEBREW_GITHUB_API_TOKEN=`head -1 ${github_token_path}`
 fi
+unset github_token_path
 
 # Export MySQL credentials if it's readable
-local mysql_credentials_path="$HOME/.my.cnf"
+mysql_credentials_path="$HOME/.my.cnf"
 if [[ -r "${mysql_credentials_path}" ]]; then
-  local user="`cat "$mysql_credentials_path" | grep "user" | cut -d= -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`"
-  local pass="`cat "$mysql_credentials_path" | grep "password" | cut -d= -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`"
+  user="`cat "$mysql_credentials_path" | grep "user" | cut -d= -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`"
+  pass="`cat "$mysql_credentials_path" | grep "password" | cut -d= -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`"
 
   if [ "x${user}" != "x" ]; then
     export MYSQL_USERNAME="${user}"
     export MYSQL_PASSWORD="${pass}"
   fi
+  unset user pass
 fi
+unset mysql_credentials_path
