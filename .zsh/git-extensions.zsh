828a7c5d2a34d7d659d0478e97b2e68beb2e653c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-04-03 08:24:42 -0700

    any /dmx is a DMX project

--- .zsh/git-extensions.zsh
+++ .zsh/git-extensions.zsh
@@ -7,7 +7,7 @@ function gcim() {
     return 1
   fi
 
-  if echo "${PWD}" | grep -q "ads/dmx\|dailymotion/go-liverail"; then
+  if echo "${PWD}" | grep -q "/dmx\|dailymotion/go-liverail"; then
     project="[DMX] "
   fi
 
