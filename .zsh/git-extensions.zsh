9dc5f6814d39fb70ea6e807a46b8a00b105b2b83
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-04-16 21:18:48 -0700

    fix gcim to work for all of my workflows

--- .zsh/git-extensions.zsh
+++ .zsh/git-extensions.zsh
@@ -1,6 +1,16 @@
 function gcim() {
   local message="${@}"
   local project=""
+  local branch=""
+  local story=""
+
+  if [[ "x`git rev-parse --git-dir 2> /dev/null`" != "x" ]]; then
+    branch="`current_branch`"
+    story="`echo "${branch}" | grep '^\(DAILY\|DMX\)-[[:digit:]][[:digit:]]*$'`"
+  else
+    echo "You must be under a git repository to use gcim"
+    return 1
+  fi
 
   if [[ -z "${message}" ]]; then
     echo "USAGE: gcim <message>"
@@ -8,27 +18,29 @@ function gcim() {
   fi
 
   if echo "${PWD}" | grep -q "/dmx\|dailymotion/go-liverail"; then
-    project="[DMX] "
+    project="DMX"
   fi
 
-  if [[ -z "${DM_PROJECT}" ]]; then
-    if [[ -z "${project}" ]]; then
-      echo "Missing project, please export the project name as DM_PROJECT and amend the commit if needed"
-    fi
-  else
-    project="[${DM_PROJECT}] "
+  if [[ -n "${PROJECT}" ]]; then
+    project="${PROJECT} "
   fi
 
-  if [[ "x`git rev-parse --git-dir 2> /dev/null`" != "x" ]]; then
-    local branch="`current_branch`"
-    local story="`echo "${branch}" | grep '^\(DAILY\|DMX\)-[[:digit:]][[:digit:]]*$'`"
-    if [[ "x${story}" != "x" ]]; then
-      git commit -m "${project}${message} (${story})"
-    else
-      git commit -m "${project}${message}"
-    fi
+  if [[ -z "${project}" ]]; then
+    project="`echo "${branch}" | grep '^[Uu][Ss][0-9]*[-_]' \
+      | sed -e 's:^\([Uu][Ss][0-9]*\)[-_].*:\1:g' \
+      | tr 'a-z' 'A-Z'`"
+  fi
+
+  if [[ -z "${project}" ]]; then
+    echo "Missing project, please export the project name as PROJECT and amend the commit if needed"
   else
-    echo "You must be under a git repository to use gcim"
-    return 1
+    project="[${project}] "
+  fi
+
+
+  if [[ "x${story}" != "x" ]]; then
+    git commit -m "${project}${message} (${story})"
+  else
+    git commit -m "${project}${message}"
   fi
 }
