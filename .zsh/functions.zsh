7bd99b3d43b2c958c94b16ff7ca672be59f6a4c7
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-04-24 14:22:20 -0700

    add devnfs to switch to loading through NFS

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -589,7 +589,12 @@ function pathmunge() {
 }
 #}}}
 # nse()#{{{
-function nse () {
+function nse (){
     docker exec -it $1 bash
+}
+#}}}
+# devnfs() #{{{
+function devnfs() {
+    docker-machine ssh dev 'export IP="$(netstat -rn | grep eth1 | awk "{print $1}" | cut -d. -f1-3).1" && sudo umount /Users && sudo /usr/local/etc/init.d/nfs-client start >/dev/null && sudo mount $IP:/Users /Users -o rw,async,noatime,rsize=32768,wsize=32768,proto=tcp && echo "Mounted /Users over NFS"'
 }
 #}}}
