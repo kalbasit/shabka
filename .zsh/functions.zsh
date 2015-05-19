37ffec67a957ce5eb2acfc13e7ce3f5f7e70376e
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-05-19 09:23:43 -0700

    Make devnfs more readable

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -595,7 +595,12 @@ function nse (){
 #}}}
 # devnfs() #{{{
 function devnfs() {
-    docker-machine ssh dev 'export IP="$(netstat -rn | grep eth1 | awk "{print $1}" | cut -d. -f1-3).1" && sudo umount /Users && sudo /usr/local/etc/init.d/nfs-client start >/dev/null && sudo mount $IP:/Users /Users -o rw,async,noatime,rsize=32768,wsize=32768,proto=tcp && echo "Mounted /Users over NFS"'
+  docker-machine ssh dev '\
+  export IP="$(netstat -rn | grep eth1 | awk "{print $1}" | cut -d. -f1-3).1" \
+  && sudo umount /Users \
+  && sudo /usr/local/etc/init.d/nfs-client start >/dev/null \
+  && sudo mount $IP:/Users /Users -o rw,async,noatime,rsize=32768,wsize=32768,proto=tcp \
+  && echo "Mounted /Users over NFS"'
 }
 #}}}
 # swp() #{{{
