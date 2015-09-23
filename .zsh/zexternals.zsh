c87ac1a9d44892b1dcc14ae2b158d0ff19bdc503
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-09-23 13:47:20 -0700

    tell CGO to use libraries installed by Homebrew

--- .zsh/zexternals.zsh
+++ .zsh/zexternals.zsh
@@ -1,6 +1,12 @@
 if [[ -x $(which brew 2>/dev/null) ]]; then
   # Load autojump
   [[ -s "$(brew --prefix)/etc/profile.d/autojump.sh" ]] && source "$(brew --prefix)/etc/profile.d/autojump.sh"
+
+  # Export CFLAGS and LDFLAGS
+  export CGO_CFLAGS="-I/usr/local/include"
+  export CGO_CPPFLAGS="${CGO_CFLAGS}"
+  export CGO_CXXFLAGS="${CGO_CFLAGS}"
+  export CGO_LDFLAGS="-L/usr/local/lib"
 fi
 
 # Load travis
