d5fcaa2af12c747e388c71288e3ab6fd985ec3fe
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-10-30 21:36:50 -0700

    functions: take a few useful functions from mathias

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -710,3 +710,28 @@ function machdev() {
     fi
 }
 #}}}
+# calc() #{{{
+# Taken from https://github.com/mathiasbynens/dotfiles/blob/master/.functions
+function calc() {
+	local result="";
+	result="$(printf "scale=10;$*\n" | bc --mathlib | tr -d '\\\n')";
+	#                       └─ default (when `--mathlib` is used) is 20
+	#
+	if [[ "$result" == *.* ]]; then
+		# improve the output for decimal numbers
+		printf "$result" |
+		sed -e 's/^\./0./'        `# add "0" for cases like ".5"` \
+		    -e 's/^-\./-0./'      `# add "0" for cases like "-.5"`\
+		    -e 's/0*$//;s/\.$//';  # remove trailing zeros
+	else
+		printf "$result";
+	fi;
+	printf "\n";
+}
+# }}}
+# mkd() {{{
+# Taken from https://github.com/mathiasbynens/dotfiles/blob/master/.functions
+function mkd() {
+    mkdir -p "$@" && cd "$_";
+}
+# }}}
