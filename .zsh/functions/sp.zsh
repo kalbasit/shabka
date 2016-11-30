7466dff0f37d5bc118d3e3ec123b301eda314b11
Wael M. Nasreddine <wael@getpublica.com>
2016-11-30 09:38:24 -0800

    sp: allow the configuration of the log level

--- .zsh/functions/sp.zsh
+++ .zsh/functions/sp.zsh
@@ -1,45 +1,58 @@
-#
-# vim:ft=sh:fenc=UTF-8:ts=4:sts=4:sw=4:expandtab:foldmethod=marker:foldlevel=0:
-#
-# Some functions are taken from
-#       http://phraktured.net/config/
-#       http://www.downgra.de/dotfiles/
-
-# sp() #{{{
+# sp stands for switch profile and it is designed to change the work profile of
+# the terminal by calling the appropriate pactivate() and pdeactivate() on the
+# profile
 function sp() {
-    profiles=()
-    for wpf in ${HOME}/.zsh/profiles/*.zsh; do
-        f="`basename "${wpf}"`"
+    local profiles=()
+    local requested_profile="${1}"
+    local log_depth="${2}"
+    local pf=
+
+    # sanity check
+    if ! [[ "${log_depth}" -ge 0 ]]; then
+        log_depth=0
+    fi
+    # load the available profiles
+    for pf in ${HOME}/.zsh/profiles/*.zsh; do
+        f="$(basename "${pf}")"
         profiles=(${profiles[@]} ${f%%.zsh})
     done
-    if [ "${#}" -eq "0" -o "${1}" = "ls" ]; then
-        for wpf in ${profiles[@]}; do
-            if [[ "x${wpf}" = "x${ACTIVE_PROFILE}" ]]; then
-                echo "${FG_GREEN}*${FG_CLEAR} ${wpf}"
+    # are we only trying to list the available profiles?
+    if [[ "${#}" -eq "0" ]] || [[ "${requested_profile}" = "ls" ]]; then
+        for pf in "${profiles[@]}"; do
+            if [[ "x${pf}" = "x${ACTIVE_PROFILE}" ]]; then
+                print_info "${log_depth}" "${FG_GREEN}*${FG_CLEAR} ${pf}"
             else
-                echo "  ${wpf}"
+                print_info "${log_depth}" "  ${pf}"
             fi
         done
-    elif [[ "${1}" = "kill" ]]; then
+    # are we loading the personal profile (i.e removing profiles?)
+    elif [[ "${requested_profile}" = "kill" ]] || [[ "${requested_profile}" = "personal" ]]; then
         if [[ -n "${ACTIVE_PROFILE}" ]]; then
+            # shellcheck disable=SC1090
             source "${HOME}/.zsh/profiles/${ACTIVE_PROFILE}.zsh"
             pdeactivate
             unset ACTIVE_PROFILE SSH_AGENT_PID SSH_AUTH_SOCK SSH_AGENT_NAME
-            eval `ssh-agents $SHELL`
+            eval "$(ssh-agents "$SHELL")"
         fi
+    # are we actually loading a profile?
     else
-        if [[ ! -e "${HOME}/.zsh/profiles/${1}.zsh" ]]; then
-            echo "profile ${1} not found."
+        if [[ ! -e "${HOME}/.zsh/profiles/${requested_profile}.zsh" ]]; then
+            print_error "${log_depth}" "profile ${HOME}/.zsh/profiles/${requested_profile}.zsh not found"
             return 1
         fi
 
+        # unload the current profile
         sp kill
-        source "${HOME}/.zsh/profiles/${1}.zsh"
+        # activate the profile
+        # shellcheck disable=SC1090
+        source "${HOME}/.zsh/profiles/${requested_profile}.zsh"
         pactivate
-        export ACTIVE_PROFILE="${1}"
-        export SSH_AGENT_NAME="${1}"
+        # let the terminal know whick profile is loaded and which ssh-agent to
+        # use
+        export ACTIVE_PROFILE="${requested_profile}"
+        export SSH_AGENT_NAME="${requested_profile}"
         unset SSH_AGENT_PID SSH_AUTH_SOCK
-        eval `ssh-agents $SHELL`
+        eval "$(ssh-agents "$SHELL")"
     fi
 }
 #}}}
