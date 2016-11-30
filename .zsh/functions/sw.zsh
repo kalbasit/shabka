ed46f4a69669091e2e21219c4bdeacea9d845ac6
Wael M. Nasreddine <wael@getpublica.com>
2016-11-30 09:39:02 -0800

    sw: redo sw() to parse an rc file instead of hardcoding the paths/profiles

--- .zsh/functions/sw.zsh
+++ .zsh/functions/sw.zsh
@@ -1,35 +1,35 @@
-#
-# vim:ft=sh:fenc=UTF-8:ts=4:sts=4:sw=4:expandtab:foldmethod=marker:foldlevel=0:
-#
-# Some functions are taken from
-#       http://phraktured.net/config/
-#       http://www.downgra.de/dotfiles/
-
-# sw() {{{
+# sw stands for Start Working and it basically ensures that all of the projects
+# that I need are open in a tmux session
 function sw() {
     local current_profile=
-    local repo=
+    local dir=
+    local i=
+    local profile=
+    local rc="${HOME}/.swrc"
+    local rc_length=
+
+    # compute the length of the rc file
+    rc_length="$( jq length "${rc}" )"
     # record the current profile
     current_profile="${ACTIVE_PROFILE}"
-    # start personal
-    sp kill
-    tmx -n -d "${HOME}/.dotfiles"
-    for repo in github.com/kalbasit/{advantage2,tf-k8s,tf-vpc,tf-etcd,terraform}; do
-        tmx -n -d "$GOPATH/src/${repo}"
-    done
-    # start publica
-    sp publica
-    for repo in github.com/publica-project/{adctrl,bootstrap-js,console,console-api,demo,deploy-tools,emr,grpc-logger,homepage,js-app,js-utils,kube-manifests,pig,protobuf-defs,request-logger,storm-s3,terraform,videojs-publica-ads,website}; do
-        tmx -n -d "$GOPATH/src/${repo}"
-    done
-    # start dailymotion
-    sp kill
-    for repo in github.com/dailymotion/{ad-controller,player}; do
-        tmx -n -d "$GOPATH/src/${repo}"
+    for (( i = 0; i < rc_length; i++)); do
+        profile="$( jq -r ".[${i}] .profile" "${rc}" )"
+        # TODO(kalbasit): I cannot figure out how to interpolate variables
+        # stored in the dir variable in rc. i.e it's always
+        # $GOPATH/src/github.com/... and I cannot turn $GOPATH to the value
+        dir="$( jq -r ".[${i}] .dir" "${rc}" | sed -e "s:\$GOPATH:$GOPATH:g" -e "s:\$HOME:$HOME:g" )"
+        if [[ -z "${profile}" ]]; then
+            print_error 0 "profile cannot be empty"
+            print_error 1 "$(jq -r .[${i}] "${rc}" )"
+        fi
+        print_info 0 "starting ${dir} under the ${profile} profile"
+        sp "${profile}" 2
+        tmx --log-depth 2 --ignore-if-session-exists --new --start-detached "${dir}"
     done
     # restore the current profile
     if [[ -n "${current_profile}" ]]; then
         sp "${current_profile}"
     fi
+    # open the dotfiles tmux
+    tmx "${HOME}/.dotfiles"
 }
-# }}}
