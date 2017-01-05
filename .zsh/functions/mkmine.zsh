e8b77bc05da6e52fa1aa28c88ba96f6cbb69f4a5
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-01-04 22:04:26 -0800

    zsh/(): set the filetype to zsh

--- .zsh/functions/mkmine.zsh
+++ .zsh/functions/mkmine.zsh
@@ -1,5 +1,5 @@
 #
-# vim:ft=sh:fenc=UTF-8:ts=4:sts=4:sw=4:expandtab:foldmethod=marker:foldlevel=0:
+# vim:ft=zsh:fenc=UTF-8:ts=4:sts=4:sw=4:expandtab:foldmethod=marker:foldlevel=0:
 #
 # Some functions are taken from
 #       http://phraktured.net/config/
