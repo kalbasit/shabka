51cc471e5afecebec3dcf6f8331257c934edfa81
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-04-20 12:55:23 -0700

    zsh: remove the gl alias as it is assigned to github_commit_link now

--- .zsh/plugins/git/git.plugin.zsh
+++ .zsh/plugins/git/git.plugin.zsh
@@ -147,8 +147,9 @@ alias gd='git diff'
 compdef _git gd=git-diff
 alias gdc='git diff --cached'
 compdef _git gdc=git-diff
-alias gl='git pull'
-compdef _git gl=git-pull
+# remove gl alias to replace it with git link (gl)
+# alias gl='git pull'
+# compdef _git gl=git-pull
 alias gup='git pull --rebase'
 compdef _git gup=git-fetch
 alias gp='git push'
