7f00fad36b2950cec921e2fc17a066c3650f44b7
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-05-17 12:12:22 -0700

    zsh: clone() did not care about argument

--- .zsh/functions/clone.zsh
+++ .zsh/functions/clone.zsh
@@ -1,12 +1,12 @@
 function clone() { # URL
   local url="${1}"
   # compute the GOPATH format of the URL of origin
-  local gfrp="$( git_gopath_formatted_repo_path )"
+  local gfrp="$( git_gopath_formatted_repo_path "${url}" )"
   # compute the path of the clone
   local clone_path="${GOPATH}/src/${gfrp}"
   # make sure we do not clone an already cloned repo
   if [[ -e "${clone_path}" ]]; then
-    print_error "${url} is already cloned at ${clone_path}"
+    print_error 0 "${clone_path} already exists"
     return 1
   fi
   # make sure the directory is avalable
--- .zsh/functions/git_gopath_formatted_repo_path.zsh
+++ .zsh/functions/git_gopath_formatted_repo_path.zsh
@@ -1,5 +1,7 @@
 function git_gopath_formatted_repo_path() {
-  git remote get-url origin | \
+  repo_url="${1}"
+  [[ -z "${repo_url}" ]] && repo_url="$( git remote get-url origin )"
+  echo "${repo_url}" | \
     sed -e 's#^\(http://\|https://\|git@\)\(.*\)\(\.git\)$#\2#g' | \
     tr ':' '/'
 }
