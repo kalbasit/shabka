d24ff70abc68fb7ff6b68fabefe261c129cd2e85
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-11-13 16:40:57 -0800

    zsh: rsync_code should not exclude pkg to avoid errors removing empty stories

--- .zsh/hosts/cratos.zsh
+++ .zsh/hosts/cratos.zsh
@@ -1,2 +1,2 @@
 # rsync_code sends the code from cratos to zeus
-alias rsync_code='rsync -avuz --delete --exclude=.snapshots/ --exclude=pkg/ ~/code/ zeus:~/code/'
+alias rsync_code='rsync -avuz --delete --exclude=.snapshots/ ~/code/ zeus:~/code/'
