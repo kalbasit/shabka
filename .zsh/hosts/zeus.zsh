28147132e899893a5f3cb0493df13dfdbcf9ea5a
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-11-13 16:46:13 -0800

    zsh: apollo (Synology) requires using /usr/bin/rsync binary instead of /bin/rsync

--- .zsh/hosts/zeus.zsh
+++ .zsh/hosts/zeus.zsh
@@ -1,2 +1,2 @@
 # rsync_code sends the code from zeus to apollo
-alias rsync_code='rsync -avuz --delete --exclude=.snapshots/ ~/code/ apollo:/volume1/Code/active/'
+alias rsync_code='rsync -avuz --rsync-path=/usr/bin/rsync --delete --exclude=.snapshots/ ~/code/ apollo:/volume1/Code/active/'
