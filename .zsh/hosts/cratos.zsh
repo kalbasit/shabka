957acfe7c36d723fd2cef90cbb5a01348d5d7d5b
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-12-12 12:23:55 -0800

    zsh: get rid of zeus specific settings

--- .zsh/hosts/cratos.zsh
+++ .zsh/hosts/cratos.zsh
@@ -1,7 +1,3 @@
-# send_code_zeus sends the code from cratos to zeus
-alias send_code_zeus='rsync -avuz --delete --exclude=.snapshots/ ~/code/ zeus:~/code/'
-# get_code_zeus sends the code from zeus to cratos
-alias get_code_zeus='rsync -avuz --delete --exclude=.snapshots/ zeus:~/code/ ~/code/'
 # send_code_apollo sends the code from cratos to apollo
 alias send_code_apollo='rsync -avuz --rsync-path=/usr/bin/rsync --delete --exclude=.snapshots/ ~/code/ apollo:/volume1/Code/active/'
 # get_code_apollo sends the code from apollo to cratos
