2a69274f7ffbd82f22bb67efc88d9542c1588d70
Wael Nasreddine <wael.nasreddine@gmail.com>
2018-07-05 23:08:06 -0700

    git: import the config from my dotfiles
    Imports:
    - [gitconfig](https://github.com/kalbasit/dotfiles/blob/master/.gitconfig)
    - [gitignore_global](https://github.com/kalbasit/dotfiles/blob/master/.gitignore_global)
    
    TODO:
    - Substitute MSMTP binary [here](https://github.com/kalbasit/dotfiles/blob/14109dfb1f46de0ad75cc92bb9a7230f65f54914/.gitconfig#L105)
    - Substitute git-lfs binary [here](https://github.com/kalbasit/dotfiles/blob/14109dfb1f46de0ad75cc92bb9a7230f65f54914/.gitconfig#L120)
    - Binaries in the aliases [here](https://github.com/kalbasit/dotfiles/blob/14109dfb1f46de0ad75cc92bb9a7230f65f54914/.gitconfig#L30-L75)
    - Once support for secret files is added, substitue ~/.gitconfig_secrets.

---
+++ git-config/default.nix
@@ -0,0 +1,16 @@
+{stdenv, my_nvim}:
+
+stdenv.mkDerivation rec {
+  name = "git-config";
+
+  phases = [ "installPhase" ];
+
+  src = ./.;
+
+  installPhase = ''
+    install -dm 755 $out/userHome
+    substitute $src/gitconfig $out/userHome/.gitconfig \
+      --subst-var-by nvim_bin ${my_nvim}/bin/nvim
+    cp $src/gitignore_global $out/userHome/.gitignore_global
+  '';
+}
