72a967d2f68dd939e8ced194dcf68702f22eed6a
Wael Nasreddine <wael.nasreddine@gmail.com>
2018-07-06 12:12:54 -0700

    most: import config from my dotfiles
    Imports:
    
    - [.mostrc](https://github.com/kalbasit/dotfiles/blob/14109dfb1f46de0ad75cc92bb9a7230f65f54914/.mostrc)

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ echo "Installing nix profile"
 nix-env -i all
 
 if [[ -d "${HOME}/.nix-profile/userHome" ]]; then
-	pushd $HOME > /dev/null
+	pushd "${HOME}" > /dev/null
 
 	echo "Creating dotfiles links in user home"
-	find .nix-profile/userHome/ -maxdepth 1 | sed "s/.nix-profile\/userHome\///g" | \
+	find .nix-profile/userHome/ -maxdepth 1 | sed 's#.nix-profile/userHome/##g' | \
 			grep -v "^$" | xargs -I {} ln -sf .nix-profile/userHome/{} {}
 
 	popd > /dev/null
