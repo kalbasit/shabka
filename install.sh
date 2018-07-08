6df9266bdc2bfc07d1eafffc495ecaf5ea17cae0
Wael Nasreddine <wael.nasreddine@gmail.com>
2018-07-07 22:50:21 -0700

    arch: automate installation and configuration of running system

--- install.sh
+++ install.sh
@@ -5,22 +5,17 @@ echo "Installing nix profile"
 nix-env -i all
 
 if [[ -d "${HOME}/.nix-profile/userHome" ]]; then
-	pushd "${HOME}" > /dev/null
-
 	echo "Creating dotfiles links in user home"
+	pushd "${HOME}" > /dev/null
 	for f in $( find -L .nix-profile/userHome -type f | sed 's#.nix-profile/userHome/##g' ); do
 		mkdir -p "${HOME}/$( dirname "${f}" )"
 		ln -sf "${HOME}/.nix-profile/userHome/${f}" "${HOME}/${f}"
 	done
-
-	# NOTE: This is needed because Termite could not find the xterm-termite terminfo
-	# See https://github.com/NixOS/nixpkgs/issues/19785
-	if [[ ! -L .terminfo ]]; then
-		echo "Linking terminfo to allow terminals to find installed terminfo"
-		ln -sf .nix-profile/share/terminfo .terminfo
-	fi
-
 	popd > /dev/null
+fi
+
+if [[ -f /etc/arch-release ]]; then
+	./arch.sh
 fi
 
 echo "Done"
