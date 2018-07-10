6d1fd504aeea4ea868c4ddaa1956eb99f59fbd41
Wael Nasreddine <wael.nasreddine@gmail.com>
2018-07-09 17:28:20 -0700

    init: subscribe to the correct channel on NixOS
    The unstable channel for NixOS is `https://nixos.org/channels/nixos-unstable` while the unstable channel for Nix on other distributions is `https://nixos.org/channels/nixpkgs-unstable`.

--- init.sh
+++ init.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Subscribing to the unstable channel"
-if ! nix-channel --list | grep -q /nixpkgs-unstable; then
-	nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+if ! nix-channel --list | grep -q -- -unstable; then
+	echo "Subscribing to the unstable channel"
+
+	if grep -q NixOS /etc/os-release; then
+		nix-channel --add https://nixos.org/channels/nixos-unstable
+	else
+		nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+	fi
+
+	nix-channel --update
 fi
