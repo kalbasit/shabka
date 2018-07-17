ddeb4e92c3c0cccd5785ddbbee6556c3120cd6ea
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-17 00:42:28 -0700

    nixos/publica: install Aerospike, Memcache, MySQL and Redis
    TODO: enable Aerospike once nixos-unstable moves past dae9cf6106d

--- machines/imports/publica.nix
+++ machines/imports/publica.nix
@@ -5,4 +5,25 @@
   networking.extraHosts = ''
     127.0.0.1 api.publica.dev console.publica.dev home.publica.dev ctrl.publica.dev js.publica.dev rewriter.publica.dev publica.dev
   '';
+
+  # -~ HACK ~-
+  # Workaround lack of kubes by installing Aerospike, Memcache, MySQL and Redis.
+  # TODO: enable this once nixos-unstable advances past dae9cf6106d
+  #services.aerospike = {
+  #  enable = true;
+  #  extraConfig = ''
+  #    namespace test {
+  #      replication-factor 2
+  #      memory-size 4G
+  #      default-ttl 1h
+  #      storage-engine memory
+  #    }
+  #  '';
+  #};
+  services.memcached.enable = true;
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+  };
+  services.redis.enable = true;
 }
