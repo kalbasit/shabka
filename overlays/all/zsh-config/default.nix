63959f3b01e6869d08c65d862bf6411a8639f773
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-21 13:42:59 -0700

    zsh-config: setup thefuck and fzf

--- overlays/all/zsh-config/default.nix
+++ overlays/all/zsh-config/default.nix
@@ -9,6 +9,7 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     install -dm 755 $out/userHome
+
     cp -dr $src/bin $out/bin
     cp -dr $src/libexec $out/libexec
     cp -dr $src/zsh $out/zsh
@@ -17,8 +18,12 @@ stdenv.mkDerivation rec {
       --subst-var-by out_dir $out
 
     substitute $src/zshrc $out/userHome/.zshrc \
+      --subst-var-by ag_bin ${pkgs.silver-searcher}/bin/ag \
+      --subst-var-by direnv_dir ${pkgs.direnv} \
+      --subst-var-by fzf_out ${pkgs.fzf} \
+      --subst-var-by git_bin ${pkgs.git}/bin/git \
       --subst-var-by out_dir $out \
-      --subst-var-by direnv_dir ${pkgs.direnv}
+      --subst-var-by thefuck_out ${pkgs.thefuck}
 
     substituteInPlace $out/zsh/functions/tmycli \
       --subst-var-by mycli_bin ${pkgs.mycli}/bin/mycli \
