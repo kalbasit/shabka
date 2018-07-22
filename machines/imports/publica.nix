c453c182ee67e882078e26a3deb8ca70ebc7704c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-21 23:17:24 -0700

    machines/imports/publica: remove the services, Minikube now works

--- machines/imports/publica.nix
+++ machines/imports/publica.nix
@@ -4,26 +4,6 @@
   # Add the extra hosts
   networking.extraHosts = ''
     127.0.0.1 api.publica.dev console.publica.dev home.publica.dev ctrl.publica.dev js.publica.dev rewriter.publica.dev publica.dev
-    127.0.0.1 k8s.publica.dev
+    192.168.99.100 k8s.publica.dev
   '';
-
-  # -~ HACK ~-
-  # Workaround lack of kubes by installing Aerospike, Memcache, MySQL and Redis.
-  services.aerospike = {
-    enable = true;
-    extraConfig = ''
-      namespace test {
-        replication-factor 2
-        memory-size 4G
-        default-ttl 1h
-        storage-engine memory
-      }
-    '';
-  };
-  services.memcached.enable = true;
-  services.mysql = {
-    enable = true;
-    package = pkgs.mariadb;
-  };
-  services.redis.enable = true;
 }
