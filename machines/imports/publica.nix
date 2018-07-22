d271a3149441ad630c070014391d69b94794d8c1
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-21 20:17:58 -0700

    machines/imports/publica: point k8s.publica.dev to 127.0.0.1

--- machines/imports/publica.nix
+++ machines/imports/publica.nix
@@ -4,6 +4,7 @@
   # Add the extra hosts
   networking.extraHosts = ''
     127.0.0.1 api.publica.dev console.publica.dev home.publica.dev ctrl.publica.dev js.publica.dev rewriter.publica.dev publica.dev
+    127.0.0.1 k8s.publica.dev
   '';
 
   # -~ HACK ~-
