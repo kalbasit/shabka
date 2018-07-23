36445a77d0a5a0c89fe017b5bfbac8dd3fdf4702
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-22 21:45:09 -0700

    neovim: override neovim-unwrapped to fix a bug in the clipboard provider
    fixes #17

--- overlays/neovim/default.nix
+++ overlays/neovim/default.nix
@@ -87,4 +87,12 @@ in
       ];
     };
   };
+
+  # TODO: remove the following override once the below PR is merged.
+  # - https://github.com/NixOS/nixpkgs/pull/43995
+  neovim-unwrapped = super.neovim-unwrapped.overrideAttrs(oa: {
+    postInstall = ''
+      sed -i -e "s|'xsel|'${super.xsel}/bin/xsel|g" $out/share/nvim/runtime/autoload/provider/clipboard.vim
+    '';
+  });
 }
