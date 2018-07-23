ada3376cf33b3e7722c0666c0d91e15906411886
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-22 19:01:36 -0700

    machines/imports/redshift: inline the services.redshift settings

--- machines/imports/redshift.nix
+++ machines/imports/redshift.nix
@@ -1,11 +1,9 @@
 { config, pkgs, lib, ... }:
 
 {
-  services.redshift = {
-    enable = true;
-    latitude = lib.mkDefault "34.42";
-    longitude = lib.mkDefault "-122.11";
-    temperature.day = 5900;
-    temperature.night = 3700;
-  };
+  services.redshift.enable = true;
+  services.redshift.latitude = "34.42";
+  services.redshift.longitude = "-122.11";
+  services.redshift.temperature.day = 5900;
+  services.redshift.temperature.night = 3700;
 }
