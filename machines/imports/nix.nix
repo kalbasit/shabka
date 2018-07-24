fc25e43c3c0f60fd9f510a9cd98b4b039cea0430
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-23 18:10:17 -0700

    nix: optimise the store daily at 12:00

--- machines/imports/nix.nix
+++ machines/imports/nix.nix
@@ -10,13 +10,21 @@ in
     buildCores = 0;
     daemonIONiceLevel = 7;
     daemonNiceLevel = 10;
+
     extraOptions = ''
       auto-optimise-store = true
     '';
+
     nixPath = [
       "nixpkgs=${system-path}/external/nixpkgs"
       "nixos-config=${system-path}/machines/${config.networking.hostName}/configuration.nix"
     ];
+
+    optimise = {
+      automatic = true;
+      dates = [ "12:00" ];
+    };
+
     useSandbox = true;
   };
 }
