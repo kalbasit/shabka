731e28e4e61bf5d83005ff1283aa2e7db2ebc190
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-24 13:13:08 -0700

    overlays: add TODO comments about overlays filed upstream

--- overlays/amazon-ecr-credential-helper/default.nix
+++ overlays/amazon-ecr-credential-helper/default.nix
@@ -1,5 +1,6 @@
 self: super:
 
 {
+  # TODO: remove this once https://github.com/NixOS/nixpkgs/pull/44050 is merged
   amazon-ecr-credential-helper = import ./go-package.nix { inherit (super) stdenv buildGoPackage fetchgit fetchhg fetchbzr fetchsvn; };
 }
--- overlays/pet/default.nix
+++ overlays/pet/default.nix
@@ -1,5 +1,6 @@
 self: super:
 
 {
+  # TODO: remove this once https://github.com/NixOS/nixpkgs/pull/44033 is merged
   pet = import ./go-package.nix { inherit (super) stdenv buildGoPackage fetchgit fetchhg fetchbzr fetchsvn; };
 }
