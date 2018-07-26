35d6a6b297e285980a3fee36ac0028e30fc644fa
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-26 15:27:42 -0700

    xautolock: start i3lock with a image of the galaxy
    TODO: select a better image that works best when set in tiling mode

--- modules/xorg.nix
+++ modules/xorg.nix
@@ -1,5 +1,12 @@
 { config, pkgs, lib, ... }:
 
+let
+  image = pkgs.fetchurl {
+    url = "https://i.imgur.com/7kb9cEF.png"; # hosted on my imgur
+    sha256 = "01k36djnp58cv1ik1afz8l2rmfwmmyizgjsysi84hf1rcjrqqqsq";
+  };
+in
+
 {
   services.xserver.autoRepeatDelay = 200;
   services.xserver.autoRepeatInterval = 30;
@@ -12,5 +19,5 @@
 
   # enable xautolock
   services.xserver.xautolock.enable = true;
-  services.xserver.xautolock.locker = "${pkgs.i3lock}/bin/i3lock";
+  services.xserver.xautolock.locker = "${pkgs.i3lock}/bin/i3lock -ti ${image}";
 }
