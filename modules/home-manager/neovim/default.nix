f8f709f07ebebf70bbfe9cfefcad3fa3118e3bc4
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-28 00:41:08 -0700

    why string?

--- modules/home-manager/neovim/default.nix
+++ modules/home-manager/neovim/default.nix
@@ -21,67 +21,70 @@ in {
         xsel_bin = "${pkgs.xsel}/bin/xsel";
       });
 
+      packages.myVimPackage = with pkgs.vimPlugins; {
+                # loaded on launch
+                start = [
+                  Gist
+                  Gundo
+                  LanguageClient-neovim
+                  ack-vim
+                  ale
+                  auto-pairs
+                  caw
+                  easy-align
+                  easymotion
+                  editorconfig-vim
+                  fugitive
+                  fzf-vim
+                  fzfWrapper
+                  multiple-cursors
+                  nvim-completion-manager
+                  polyglot
+                  repeat
+                  rhubarb
+                  sleuth
+                  surround
+                  vim-PreserveNoEOL
+                  vim-airline
+                  vim-airline-themes
+                  vim-better-whitespace
+                  vim-colemak
+                  vim-color-seoul256
+                  vim-csv
+                  vim-emmet
+                  vim-eunuch
+                  vim-go
+                  vim-markdown
+                  vim-pig
+                  vim-signify
+                  vim-speeddating
+                  vim-terraform
+                  vim-vissort
+                  vim-zoomwintab
+
+                  ## DeoPlete completion support
+                  deoplete-nvim
+
+                  # Golang support
+                  deoplete-go
+
+                  # Typescript support
+                  # vim-typescript    # TODO: https://github.com/kalbasit/dotfiles/issues/15
+                  vim-yats
+
+                  # required by Gist
+                  # TODO: https://github.com/NixOS/nixpkgs/pull/43399
+                  webapi-vim
+
+                  vim-maktaba
+                  vim-bazel
+                ];
+
+                # manually loadable by calling `:packadd $plugin-name`
+                opt = [ ];
+      };
+
       vam.knownPlugins = pkgs.vimPlugins // my_plugins;
-      vam.pluginDictionaries = [
-        {
-          names = [
-            "Gist"
-            "Gundo"
-            "LanguageClient-neovim"
-            "ack-vim"
-            "ale"
-            "auto-pairs"
-            "caw"
-            "easy-align"
-            "easymotion"
-            "editorconfig-vim"
-            "fugitive"
-            "fzf-vim"
-            "fzfWrapper"
-            "multiple-cursors"
-            "nvim-completion-manager"
-            "polyglot"
-            "repeat"
-            "rhubarb"
-            "sleuth"
-            "surround"
-            "vim-PreserveNoEOL"
-            "vim-airline"
-            "vim-airline-themes"
-            "vim-better-whitespace"
-            "vim-colemak"
-            "vim-color-seoul256"
-            "vim-csv"
-            "vim-emmet"
-            "vim-eunuch"
-            "vim-go"
-            "vim-markdown"
-            "vim-pig"
-            "vim-signify"
-            "vim-speeddating"
-            "vim-terraform"
-            "vim-vissort"
-            "vim-zoomwintab"
-
-            ## DeoPlete completion support
-            "deoplete-nvim"
-
-            # Golang support
-            "deoplete-go"
-
-            # Typescript support
-            # "vim-typescript"    # TODO: https://github.com/kalbasit/dotfiles/issues/15
-            "vim-yats"
-
-            # required by Gist
-            # TODO: https://github.com/NixOS/nixpkgs/pull/43399
-            "webapi-vim"
-
-            "vim-maktaba"
-            "vim-bazel"
-          ];
-        }
-      ];
     };
   };
 }
