c3dc161c7e638eb1e8f325104c957f3fc0260d99
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-07-30 15:43:41 -0700

    move most plugins

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -71,14 +71,87 @@
       thefuck_out  = "${pkgs.thefuck}";
     });
 
+    oh-my-zsh = {
+      plugins = [
+        "command-not-found"
+        "extract"
+        "git"
+        "history"
+        "kuberenetes"
+        "sudo"
+      ];
+    };
+
     plugins = [
       {
-        name = "zsh-kubernetes";
+        name = "enhancd";
+        file = "init.sh";
         src = pkgs.fetchFromGitHub {
-          owner = "Dbz";
-          repo = "zsh-kubernetes";
-          rev = "8d7045d553244f84e1c65434057cc7c2602390cd";
-          sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+          owner = "b4b4r07";
+          repo = "enhancd";
+          rev = "fd805158ea19d640f8e7713230532bc95d379ddc";
+          sha256 = "0pc19dkp5qah2iv92pzrgfygq83vjq1i26ny97p8dw6hfgpyg04l";
+        };
+      }
+
+      {
+        name = "gitit";
+        src = pkgs.fetchFromGitHub {
+          owner = "peterhurford";
+          repo = "git-it-on.zsh";
+          rev = "4827030e1ead6124e3e7c575c0dd375a9c6081a2";
+          sha256 = "01xsqhygbxmv38vwfzvs7b16iq130d2r917a5dnx8l4aijx282j2";
+        };
+      }
+
+      {
+        name = "solarized-man";
+        src = pkgs.fetchFromGitHub {
+          owner = "zlsun";
+          repo = "solarized-man";
+          rev = "a902b64696271efee95f37d45589078fdfbbddc5";
+          sha256 = "04gm4qm17s49s6h9klbifgilxv8i45sz3rg521dwm599gl3fgmnv";
+        };
+      }
+
+      # TODO: try this out and enable/remove it
+      #{
+      #  name = "zsh-autosuggestions";
+      #  src = pkgs.fetchFromGitHub {
+      #    owner = "zsh-users";
+      #    repo = "zsh-autosuggestions";
+      #    rev = "v0.4.0";
+      #    sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+      #  };
+      #}
+
+      {
+        name = "zsh-completions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-completions";
+          rev = "0.27.0";
+          sha256 = "1c2xx9bkkvyy0c6aq9vv3fjw7snlm0m5bjygfk5391qgjpvchd29";
+        };
+      }
+
+      {
+        name = "zsh-history-substring-search";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-history-substring-search";
+          rev = "v1.0.1";
+          sha256 = "0lgmq1xcccnz5cf7vl0r0qj351hwclx9p80cl0qczxry4r2g5qaz";
+        };
+      }
+
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "db6cac391bee957c20ff3175b2f03c4817253e60";
+          sha256 = "0d9nf3aljqmpz2kjarsrb5nv4rjy8jnrkqdlalwm2299jklbsnmw";
         };
       }
     ];
