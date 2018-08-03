61bc768dbe5dc9a45471b0f6664d2f5294718866
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-08-03 09:19:48 -0700

    move more settings

--- modules/home-manager/i3/default.nix
+++ modules/home-manager/i3/default.nix
@@ -4,6 +4,38 @@ let
   defaultModifier = "Mod4";
   secondModifier = "Shift";
   thirdModifier = "Mod1";
+  nosid = "--no-startup-id";
+  locker = "${pkgs.xautolock}/bin/xautolock -locknow && sleep 1";
+
+  intMonitor = "eDP-1";
+    # if myHostname == "hades"
+    # then "eDP-1"
+    # else if myHostname == "cratos"
+    # then "eDP1"
+    # else "";
+
+  intMode = "1920x1080";
+    # if myHostname == "hades"
+    # then "1920x1080"
+    # else if myHostname == "cratos"
+    # then "3200x1800"
+    # else "";
+
+  intScale = "1x1";
+    # if myHostname == "hades"
+    # then "1x1"
+    # else if myHostname == "cratos"
+    # then "0.6x0.6"
+    # else "";
+
+  extMonitor = "DP-2";
+    # if myHostname == "hades"
+    # then "DP-2"
+    # else if myHostname == "cratos"
+    # then "DP1-2"
+    # else "";
+
+  extMode = "3440x1440";
 in {
   xsession.enable = true;
 
@@ -131,26 +163,219 @@ in {
         "${defaultModifier}+apostrophe" = "exec i3-input -F '[con_mark=\"%s\"] focus' -l 1 -P 'Go to: '";
 
         # volume support
-        "XF86AudioRaiseVolume" = "exec $nosid ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ false, exec $nosid ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
-        "XF86AudioLowerVolume" = "exec $nosid ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ false, exec $nosid ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
-        "XF86AudioMute" = "exec $nosid ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
+        "XF86AudioRaiseVolume" = "exec ${nosid} ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ false, exec ${nosid} ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
+        "XF86AudioLowerVolume" = "exec ${nosid} ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ false, exec ${nosid} ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
+        "XF86AudioMute" = "exec ${nosid} ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
 
         # brightness support
-        "XF86MonBrightnessUp" = "exec $nosid ${pkgs.brightnessctl}/bin/brightnessctl s +5%";
-        "XF86MonBrightnessDown" = "exec $nosid ${pkgs.brightnessctl}/bin/brightnessctl s 5%-";
-        "${secondModifier}+XF86MonBrightnessUp" = "exec $nosid ${pkgs.brightnessctl}/bin/brightnessctl s +1%";
-        "${secondModifier}+XF86MonBrightnessDown" = "exec $nosid ${pkgs.brightnessctl}/bin/brightnessctl s 1%-";
+        "XF86MonBrightnessUp" = "exec ${nosid} ${pkgs.brightnessctl}/bin/brightnessctl s +5%";
+        "XF86MonBrightnessDown" = "exec ${nosid} ${pkgs.brightnessctl}/bin/brightnessctl s 5%-";
+        "${secondModifier}+XF86MonBrightnessUp" = "exec ${nosid} ${pkgs.brightnessctl}/bin/brightnessctl s +1%";
+        "${secondModifier}+XF86MonBrightnessDown" = "exec ${nosid} ${pkgs.brightnessctl}/bin/brightnessctl s 1%-";
 
         # sleep support
-        "XF86PowerOff" = "exec $nosid $locker && systemctl suspend";
+        "XF86PowerOff" = "exec ${nosid} ${locker} && systemctl suspend";
 
         # clipboard history
         "${defaultModifier}+${thirdModifier}+c" = "exec ${pkgs.rofi}/bin/rofi -modi \"clipboard:${pkgs.haskellPackages.greenclip}/bin/greenclip print\" -show clipboard";
 
         # Terminals
-        "$mod+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
-        "$mod+${secondModifier}+Return" = "exec ${pkgs.termite}/bin/termite";
+        "${defaultModifier}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
+        "${defaultModifier}+${secondModifier}+Return" = "exec ${pkgs.termite}/bin/termite";
+
+        # Modes
+        "${defaultModifier}+${thirdModifier}+r" = "mode resize";
+        "${defaultModifier}+${thirdModifier}+m" = "mode move";
       };
+
+      colors = {
+        focused = {
+          border = "#5f865f"; background = "#5f865f"; text = "#e4e4e4";
+          indicator = "#ffafaf"; childBorder = "#285577";
+        };
+
+        focusedInactive = {
+          border = "#4e4e4e"; background = "#4e4e4e"; text = "#d0d0d0";
+          indicator = "#ffafaf"; childBorder = "#5f676a";
+        };
+
+        unfocused = {
+          border = "#4e4e4e"; background = "#4e4e4e"; text = "#87d7d7";
+          indicator = "#87af87"; childBorder = "#222222";
+        };
+
+        urgent = {
+          border = "#ff0000"; background = "#ff0000"; text = "#e4e4e4";
+          indicator = "#d68787"; childBorder = "#900000";
+        };
+      };
+
+      modes = {
+        resize = {
+          # Micro resizement
+          "Control+n" = "resize shrink width 10 px or 1 ppt";
+          "Control+e" = "resize grow height 10 px or 1 ppt";
+          "Control+i" = "resize shrink height 10 px or 1 ppt";
+          "Control+o" = "resize grow width 10 px or 1 ppt";
+
+          # Normal resizing
+          "n" = "resize shrink width 50 px or 5 ppt";
+          "e" = "resize grow height 50 px or 5 ppt";
+          "i" = "resize shrink height 50 px or 5 ppt";
+          "o" = "resize grow width 50 px or 5 ppt";
+
+          # Macro resizing
+          "${secondModifier}+n" = "resize shrink width 100 px or 10 ppt";
+          "${secondModifier}+e" = "resize grow height 100 px or 10 ppt";
+          "${secondModifier}+i" = "resize shrink height 100 px or 10 ppt";
+          "${secondModifier}+o" = "resize grow width 100 px or 10 ppt";
+
+          # back to normal: Enter or Escape
+          "Return" = "mode default";
+          "Escape" = "mode default";
+        };
+
+        move = {
+          # Micro movement
+          "Control+n" = "move left 10 px";
+          "Control+e" = "move down 10 px";
+          "Control+i" = "move up 10 px";
+          "Control+o" = "move right 10 px";
+
+          # Normal resizing
+          "n" = "move left 50 px";
+          "e" = "move down 50 px";
+          "i" = "move up 50 px";
+          "o" = "move right 50 px";
+
+          # Macro resizing
+          "${secondModifier}+n" = "move left 100 px";
+          "${secondModifier}+e" = "move down 100 px";
+          "${secondModifier}+i" = "move up 100 px";
+          "${secondModifier}+o" = "move right 100 px";
+
+          # back to normal: Enter or Escape
+          "Return" = "mode default";
+          "Escape" = "mode default";
+        };
+      };
+
+      bars = [
+        {
+          position = "top";
+
+          # disable clicking on workspace buttons
+          # TODO: move this to the i3 module via PR
+          # bindsym button1 nop
+
+          colors = {
+            background = "#626262";
+            statusline = "#00ff00";
+            separator = "#d68787";
+            focusedWorkspace = { border = "#e4e4e4"; background = "#5f865f"; text = "#e4e4e4"; };
+            activeWorkspace = { border = "#e4e4e4"; background = "#ffafaf"; text = "#e4e4e4"; };
+            inactiveWorkspace = { border = "#87af87"; background = "#87af87"; text = "#626262"; };
+            urgentWorkspace = { border = "#ff0000"; background = "#ff0000"; text = "#e4e4e4"; };
+          };
+        }
+      ];
     };
+
+    extraConfig = ''
+      # keep the urgency border of a window for 500ms
+      # TODO: move this to the i3 module via PR
+      force_display_urgency_hint 500 ms
+
+      # This option determines in which mode new containers on workspace level will
+      # start.
+      # TODO: move this to the i3 module via PR
+      workspace_layout tabbed
+
+
+      # Daemon launcher
+      set ${defaultModifier}e_daemon Launch: (x) Xcape, (g) Greenclip
+      mode "${defaultModifier}e_daemon" {
+        bindsym x exec ${nosid} ${pkgs.xcape}/bin/xcape -e 'Control_L=Escape', mode "default"
+        bindsym g exec ${nosid} ${pkgs.haskellPackages.greenclip}/bin/greenclip daemon, mode "default"
+
+        # back to normal: Enter or Escape
+        bindsym Return mode "default"
+        bindsym Escape mode "default"
+      }
+      bindsym ${defaultModifier}+${thirdModifier}+l mode "${defaultModifier}e_daemon"
+
+      # Window Manager mode, this mode allows me to control i3
+      set ${defaultModifier}e_wm WM: (r) Reload i3, (e) Restart i3
+      mode "${defaultModifier}e_wm" {
+        bindsym r reload; exec ${nosid} ${pkgs.libnotify}/bin/notify-send 'i3 configuration reloaded', mode "default"
+        bindsym e restart; exec ${nosid} ${pkgs.libnotify}/bin/notify-send 'i3 restarted', mode "default"
+
+        # back to normal: Enter or Escape
+        bindsym Return mode "default"
+        bindsym Escape mode "default"
+      }
+      bindsym ${defaultModifier}+${thirdModifier}+w mode "${defaultModifier}e_wm"
+
+      # Application launcher
+      set ${defaultModifier}e_apps Launch: (p) Chromium personal, (u) Chromium publica, (c) Charles, (s) Slack, (i) Irc, (w) Whatsapp, (t) Teamviewer, (m) Pulse SMS
+      mode "${defaultModifier}e_apps" {
+        bindsym p exec ${pkgs.rbrowser}/bin/rbrowser --profile personal, mode "default"
+        bindsym u exec ${pkgs.rbrowser}/bin/rbrowser --profile publica, mode "default"
+        bindsym c exec ${pkgs.charles}/bin/charles, mode "default"
+        bindsym s exec ${pkgs.slack}/bin/slack, mode "default"
+        bindsym i exec ${pkgs.alacritty}/bin/alacritty --title=irc --exec=weechat, mode "default"
+        bindsym t exec ${pkgs.teamviewer}/bin/teamviewer, mode "default"
+        # TODO: install this
+        # bindsym w exec whatsapp-web-desktop, mode "default"
+        # TODO: install Pulse and use the binary here
+        #bindsym m exec ${nosid} /opt/Pulse\ SMS/pulse-sms, mode "default"
+
+        # back to normal: Enter or Escape
+        bindsym Return mode "default"
+        bindsym Escape mode "default"
+      }
+      bindsym ${defaultModifier}+${thirdModifier}+a mode "${defaultModifier}e_apps"
+
+      # Display mode allows output/resolution selection
+      set ${defaultModifier}e_display (l) Laptop screen, (m) Multiple screen, (w) Wide screen
+      mode "${defaultModifier}e_display" {
+        bindsym l exec ${nosid} ${pkgs.xlibs.xrandr}/bin/xrandr --output ${intMonitor} --mode ${intMode} --scale ${intScale} --output ${extMonitor} --off, mode "default"
+        bindsym m exec ${nosid} ${pkgs.xlibs.xrandr}/bin/xrandr --output ${intMonitor} --mode ${intMode} --scale ${intScale} --output ${extMonitor} --primary --mode 3440x1440 --right-of ${intMonitor}, mode "default"
+        bindsym w exec ${nosid} ${pkgs.xlibs.xrandr}/bin/xrandr --output ${intMonitor} --off --output ${extMonitor} --mode ${extMode}, mode "default"
+
+        # back to normal: Enter or Escape
+        bindsym Return mode "default"
+        bindsym Escape mode "default"
+      }
+      bindsym ${defaultModifier}+${thirdModifier}+d mode "${defaultModifier}e_display"
+
+      ## Management of power
+      set ${defaultModifier}e_power System: (l) lock, (o) logout, (s) suspend, (h) hibernate, (r) reboot, (${secondModifier}+s) shutdown
+      mode "${defaultModifier}e_power" {
+        bindsym l exec ${nosid} ${locker}, mode "default"
+        bindsym o exit
+        bindsym s exec ${nosid} ${locker} && systemctl suspend, mode "default"
+        bindsym h exec ${nosid} ${locker} && systemctl hibernate, mode "default"
+        bindsym r exec ${nosid} systemctl reboot
+        bindsym ${secondModifier}+s exec ${nosid} systemctl poweroff -i
+
+        # back to normal: Enter or Escape
+        bindsym Return mode "default"
+        bindsym Escape mode "default"
+      }
+      bindsym ${defaultModifier}+${thirdModifier}+p mode "${defaultModifier}e_power"
+
+      # CPU governor selection
+      set ${defaultModifier}e_cpugovernor CPU Scaling governor: (p) Performance, (o) Powersave
+      mode "${defaultModifier}e_cpugovernor" {
+        bindsym p exec ${nosid} ${pkgs.gksu}/bin/gksudo -- ${pkgs.linuxPackages.cpupower}/bin/cpupower frequency-set --governor performance, mode "default"
+        bindsym o exec ${nosid} ${pkgs.gksu}/bin/gksudo -- ${pkgs.linuxPackages.cpupower}/bin/cpupower frequency-set --governor powersave, mode "default"
+
+        # back to normal: Enter or Escape
+        bindsym Return mode "default"
+        bindsym Escape mode "default"
+      }
+      bindsym ${defaultModifier}+${thirdModifier}+g mode "${defaultModifier}e_cpugovernor"
+    '';
   };
 }
