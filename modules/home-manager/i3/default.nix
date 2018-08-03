fb5ff921b189af0ca9848ddf4b86138641a5f7e1
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-08-03 00:23:15 -0700

    fix the build

--- modules/home-manager/i3/default.nix
+++ modules/home-manager/i3/default.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 
-{
+let
+  defaultModifier = "Mod4";
+in {
   xsession.enable = true;
 
   xsession.windowManager.i3 = {
@@ -39,7 +41,7 @@
       };
 
       floating = {
-        modifier = "Mod4";
+        modifier = "${defaultModifier}";
       };
 
       focus = {
@@ -65,33 +67,33 @@
 
       keybindings = {
         # change focus
-        "${cfg.config.modifier}+n" = "focus left";
-        "${cfg.config.modifier}+e" = "focus down";
-        "${cfg.config.modifier}+i" = "focus up";
-        "${cfg.config.modifier}+o" = "focus right";
+        "${defaultModifier}+n" = "focus left";
+        "${defaultModifier}+e" = "focus down";
+        "${defaultModifier}+i" = "focus up";
+        "${defaultModifier}+o" = "focus right";
 
         # move focused window
-        "${cfg.config.modifier}+Shift+n" = "move left";
-        "${cfg.config.modifier}+Shift+e" = "move down";
-        "${cfg.config.modifier}+Shift+i" = "move up";
-        "${cfg.config.modifier}+Shift+o" = "move right";
+        "${defaultModifier}+Shift+n" = "move left";
+        "${defaultModifier}+Shift+e" = "move down";
+        "${defaultModifier}+Shift+i" = "move up";
+        "${defaultModifier}+Shift+o" = "move right";
 
         # split in horizontal orientation
-        "${cfg.config.modifier}+h" = "split h";
+        "${defaultModifier}+h" = "split h";
 
         # split in vertical orientation
-        "${cfg.config.modifier}+v" = "split v";
+        "${defaultModifier}+v" = "split v";
 
         # change focus between output
-        "${cfg.config.modifier}+$alt+o" = "focus output right";
-        "${cfg.config.modifier}+$alt+n" = "focus output left";
+        "${defaultModifier}+$alt+o" = "focus output right";
+        "${defaultModifier}+$alt+n" = "focus output left";
 
         # move workspaces between monitors
-        "${cfg.config.modifier}+Shift+$alt+o" = "move workspace to output right";
-        "${cfg.config.modifier}+Shift+$alt+n" = "move workspace to output left";
+        "${defaultModifier}+Shift+$alt+o" = "move workspace to output right";
+        "${defaultModifier}+Shift+$alt+n" = "move workspace to output left";
 
         # toggle sticky
-        "${cfg.config.modifier}+s" = "sticky toggle";
+        "${defaultModifier}+s" = "sticky toggle";
 
         # toggle tiling / floating
         "$alt+Shift+space" = "floating toggle";
@@ -100,53 +102,53 @@
         "$alt+space" = "focus mode_toggle";
 
         # enter fullscreen mode for the focused container
-        "${cfg.config.modifier}+f" = "fullscreen toggle";
+        "${defaultModifier}+f" = "fullscreen toggle";
 
         # kill focused window
-        "${cfg.config.modifier}+Shift+q" = "kill";
+        "${defaultModifier}+Shift+q" = "kill";
 
         # rbrowser
-        "${cfg.config.modifier}+b" = "exec ${pkgs.rbrowser}/bin/rbrowser";
+        "${defaultModifier}+b" = "exec ${pkgs.rbrowser}/bin/rbrowser";
 
         # rofi run
-        "${cfg.config.modifier}+r" = "exec ${pkgs.rofi}/bin/rofi -show run";
+        "${defaultModifier}+r" = "exec ${pkgs.rofi}/bin/rofi -show run";
 
         # rofi lastpass
-        "${cfg.config.modifier}+p" = "exec ${pkgs.rofi}/bin/rofi -modi lpass:rofi-lpass -show lpass";
+        "${defaultModifier}+p" = "exec ${pkgs.rofi}/bin/rofi -modi lpass:rofi-lpass -show lpass";
 
         # list open windows to switch to
         "$alt+Tab" = "exec ${pkgs.rofi}/bin/rofi -show window";
 
         # switch between the current and the previously focused one
-        "${cfg.config.modifier}+Tab" = "workspace back_and_forth";
-        "${cfg.config.modifier}+Shift+Tab" = "move container to workspace back_and_forth";
+        "${defaultModifier}+Tab" = "workspace back_and_forth";
+        "${defaultModifier}+Shift+Tab" = "move container to workspace back_and_forth";
 
         # dynamic workspaces
-        "${cfg.config.modifier}+space" = "exec ${pkgs.rofi}/bin/rofi -show i3Workspaces";
-        "${cfg.config.modifier}+Shift+space" = "exec ${pkgs.rofi}/bin/rofi -show i3MoveContainer";
-        "${cfg.config.modifier}+$alt+space" = "exec ${pkgs.rofi}/bin/rofi -show i3RenameWorkspace";
+        "${defaultModifier}+space" = "exec ${pkgs.rofi}/bin/rofi -show i3Workspaces";
+        "${defaultModifier}+Shift+space" = "exec ${pkgs.rofi}/bin/rofi -show i3MoveContainer";
+        "${defaultModifier}+$alt+space" = "exec ${pkgs.rofi}/bin/rofi -show i3RenameWorkspace";
 
         # change container layout (stacked, tabbed, toggle split)
-        "${cfg.config.modifier}+l" = "layout stacking";
-        "${cfg.config.modifier}+u" = "layout tabbed";
-        "${cfg.config.modifier}+y" = "layout toggle split";
+        "${defaultModifier}+l" = "layout stacking";
+        "${defaultModifier}+u" = "layout tabbed";
+        "${defaultModifier}+y" = "layout toggle split";
 
         # focus the parent container
-        "${cfg.config.modifier}+a" = "focus parent";
+        "${defaultModifier}+a" = "focus parent";
 
         # focus the child container
-        "${cfg.config.modifier}+d" = "focus child";
+        "${defaultModifier}+d" = "focus child";
 
         # start a region screenshot
-        "${cfg.config.modifier}+Shift+4" = "exec ${pkgs.maim}/bin/maim -s ~/Desktop/screenshot-`date +%Y-%m-%d_%H:%M:%S`.png";
+        "${defaultModifier}+Shift+4" = "exec ${pkgs.maim}/bin/maim -s ~/Desktop/screenshot-`date +%Y-%m-%d_%H:%M:%S`.png";
 
         # focus the urgent window
-        "${cfg.config.modifier}+x" = "[urgent=latest] focus";
+        "${defaultModifier}+x" = "[urgent=latest] focus";
 
         # mark current window / goto mark
         # https://github.com/tybitsfox/i3msg/blob/master/.i3/config
-        "${cfg.config.modifier}+m" = "exec i3-input -F 'mark %s' -l 1 -P 'Mark: '";
-        "${cfg.config.modifier}+apostrophe" = "exec i3-input -F '[con_mark=\"%s\"] focus' -l 1 -P 'Go to: '";
+        "${defaultModifier}+m" = "exec i3-input -F 'mark %s' -l 1 -P 'Mark: '";
+        "${defaultModifier}+apostrophe" = "exec i3-input -F '[con_mark=\"%s\"] focus' -l 1 -P 'Go to: '";
 
         # volume support
         "XF86AudioRaiseVolume" = "exec $nosid ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ false, exec $nosid ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
@@ -163,7 +165,7 @@
         "XF86PowerOff" = "exec $nosid $locker && systemctl suspend";
 
         # clipboard history
-        "${cfg.config.modifier}+$alt+c" = "exec ${pkgs.rofi}/bin/rofi -modi \"clipboard:${pkgs.haskellPackages.greenclip}/bin/greenclip print\" -show clipboard";
+        "${defaultModifier}+$alt+c" = "exec ${pkgs.rofi}/bin/rofi -modi \"clipboard:${pkgs.haskellPackages.greenclip}/bin/greenclip print\" -show clipboard";
       };
     };
   };
