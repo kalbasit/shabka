7463954984b15de7094653f37266963edd7bb263
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-08-03 12:07:31 -0700

    also move sway support into the rofi module

--- modules/home-manager/rofi/default.nix
+++ modules/home-manager/rofi/default.nix
@@ -11,12 +11,23 @@ let
       cp -dr $src/* $out/bin
     '';
   };
+
+  swaySupport = pkgs.stdenv.mkDerivation rec {
+    name = "rofi-sway-support-${version}";
+    version = "0.0.1";
+    src = ./sway-support;
+    phases = [ "installPhase" "fixupPhase" ];
+    installPhase = ''
+      install -d -m755 $out/bin
+      cp -dr $src/* $out/bin
+    '';
+  };
 in {
   programs.rofi = {
     enable = true;
 
     extraConfig = ''
-      rofi.modi: window,run,ssh,drun,i3Workspaces:${i3Support}/bin/i3-switch-workspaces,i3RenameWorkspace:${i3Support}/bin/i3-rename-workspace,i3MoveContainer:${i3Support}/bin/i3-move-container,SwayWorkspaces:sway-switch-workspaces,SwayRenameWorkspace:sway-rename-workspace,SwayMoveContainer:sway-move-container
+      rofi.modi: window,run,ssh,drun,i3Workspaces:${i3Support}/bin/i3-switch-workspaces,i3RenameWorkspace:${i3Support}/bin/i3-rename-workspace,i3MoveContainer:${i3Support}/bin/i3-move-container,SwayWorkspaces:${swaySupport}/bin/sway-switch-workspaces,SwayRenameWorkspace:${swaySupport}/bin/sway-rename-workspace,SwayMoveContainer:${swaySupport}/bin/sway-move-container
     '';
 
     font = "SourceCodePro 9";
