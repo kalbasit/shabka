eacff8b3e05476f10723ad8052ef478b5e1c3ee1
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-08-06 13:15:08 -0700

    fix the systemd unit file

--- modules/home-manager/lowbatt/default.nix
+++ modules/home-manager/lowbatt/default.nix
@@ -6,6 +6,30 @@ with lib;
 
 let
   cfg = config.services.batteryNotifier;
+  script = pkgs.writeTextFile {
+    name = "unit-script";
+    executable = true;
+    destination = "/bin/lowbatt-start";
+    text = ''
+        export battery_capacity=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${cfg.device}/capacity)
+        export battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${cfg.device}/status)
+
+        if [[ $battery_capacity -le ${builtins.toString cfg.notifyCapacity} && $battery_status = "Discharging" ]]; then
+            ${pkgs.libnotify}/bin/notify-send --urgency=critical --hint=int:transient:1 --icon=battery_empty "Battery Low" "You should probably plug-in."
+        fi
+
+        if [[ $battery_capacity -le ${builtins.toString cfg.hibernateCapacity} && $battery_status = "Discharging" ]]; then
+            ${pkgs.libnotify}/bin/notify-send --urgency=critical --hint=int:transient:1 --icon=battery_empty "Battery Critically Low" "Computer will hibernate in 60 seconds."
+            sleep 60s
+
+            battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${cfg.device}/status)
+            if [[ $battery_status = "Discharging" ]]; then
+                systemctl hibernate
+            fi
+        fi
+    '';
+  };
+
 in {
   options = {
     services.batteryNotifier = {
@@ -38,33 +62,30 @@ in {
 
   config = mkIf cfg.enable {
     systemd.user.timers."lowbatt" = {
-      description = "check battery level";
-      timerConfig.OnBootSec = "1m";
-      timerConfig.OnUnitInactiveSec = "1m";
-      timerConfig.Unit = "lowbatt.service";
-      wantedBy = ["timers.target"];
+      Unit = {
+        Description = "check battery level";
+      };
+
+      Timer = {
+        OnBootSec = "1m";
+        OnUnitInactiveSec = "1m";
+        Unit = "lowbatt.service";
+      };
+
+      Install = {
+        WantedBy = [ "timers.target" ];
+      };
     };
+
     systemd.user.services."lowbatt" = {
-      description = "battery level notifier";
-      serviceConfig.PassEnvironment = "DISPLAY";
-      script = ''
-        export battery_capacity=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${cfg.device}/capacity)
-        export battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${cfg.device}/status)
+      Unit = {
+        Description = "battery level notifier";
+      };
 
-        if [[ $battery_capacity -le ${builtins.toString cfg.notifyCapacity} && $battery_status = "Discharging" ]]; then
-            ${pkgs.libnotify}/bin/notify-send --urgency=critical --hint=int:transient:1 --icon=battery_empty "Battery Low" "You should probably plug-in."
-        fi
-
-        if [[ $battery_capacity -le ${builtins.toString cfg.hibernateCapacity} && $battery_status = "Discharging" ]]; then
-            ${pkgs.libnotify}/bin/notify-send --urgency=critical --hint=int:transient:1 --icon=battery_empty "Battery Critically Low" "Computer will hibernate in 60 seconds."
-            sleep 60s
-
-            battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${cfg.device}/status)
-            if [[ $battery_status = "Discharging" ]]; then
-                systemctl hibernate
-            fi
-        fi
-      '';
+      Service = {
+        PassEnvironment = "DISPLAY";
+        ExecStart = "${script}/bin/lowbatt-start";
+      };
     };
   };
 }
