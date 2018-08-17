e0ce0fb9f7d077cf0894887d8cf7db8f894f9405
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-08-16 20:26:53 -0700

    timewarrior: patch init.cpp to not try to write the config file if it exists

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,4 +16,12 @@ myPkgs // {
 
   # revert kernel 4.18 update due to https://github.com/NixOS/nixpkgs/issues/45165
   linuxPackages_latest = super.linuxPackages_4_17;
+
+  # timewarrior errors out if it can't write the config file, even though it's tracked by Nix
+  # If timewarrior.cfg is not writable, timew errors out with Insufficient permissions for '/home/kalbasit/.timewarrior/timewarrior.cfg'.
+  # I believe the root cause is https://github.com/GothenburgBitFactory/timewarrior/blob/004afd64a5556ba3d35fd99c14d82f9b3ca64f1b/src/init.cpp#L183-L186
+  # TODO: file a ticket upstream, and try to fix it with an overlay
+  timewarrior = super.timewarrior.overrideAttrs (oa: {
+    patches = [./timewarrior-no-write-config-file.patch];
+  });
 }
