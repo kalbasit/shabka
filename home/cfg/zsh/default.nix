d8addfc1c665123297ef6c23202eba74a8a878f3
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-08-23 14:47:21 -0700

    zsh: remove dead code, autosuggestions is already installed via enableAutosuggestions

--- home/cfg/zsh/default.nix
+++ home/cfg/zsh/default.nix
@@ -119,17 +119,6 @@ in {
         };
       }
 
-      # TODO: try this out and enable/remove it
-      #{
-      #  name = "zsh-autosuggestions";
-      #  src = pkgs.fetchFromGitHub {
-      #    owner = "zsh-users";
-      #    repo = "zsh-autosuggestions";
-      #    rev = "v0.4.0";
-      #    sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-      #  };
-      #}
-
       {
         name = "powerlevel9k";
         file = "powerlevel9k.zsh-theme";
