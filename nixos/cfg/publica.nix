24865a8bc8ff4acb5dd8e0a497cb26f1770ad120
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-09-12 14:12:23 -0700

    nixos/cfg: publica: nginx: add prebid.publica.dev to networking.extraHosts

--- nixos/cfg/publica.nix
+++ nixos/cfg/publica.nix
@@ -71,7 +71,7 @@ in
   config = lib.mkIf setupNginx {
     # Add the extra hosts
     networking.extraHosts = ''
-      127.0.0.1 k8s.publica.dev api.publica.dev console.publica.dev home.publica.dev ctrl.publica.dev js.publica.dev rewriter.publica.dev publica.dev demo.publica.dev
+      127.0.0.1 k8s.publica.dev api.publica.dev console.publica.dev home.publica.dev ctrl.publica.dev js.publica.dev rewriter.publica.dev publica.dev demo.publica.dev prebid.publica.dev
     '';
 
     # NginX configuration for publica
