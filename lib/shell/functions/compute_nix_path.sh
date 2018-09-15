25941f7b2065d5ec316dc4d4ee7dde8b06214cb9
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-09-15 10:14:11 -0700

    lib: compute_nix_path: do not compute nixos-config and nixo-hardware on non-nix OSes

--- lib/shell/functions/compute_nix_path.sh
+++ lib/shell/functions/compute_nix_path.sh
@@ -1,5 +1,5 @@
 function compute_nix_path() {
-	local nix_path=""
+	local new_nix_path=""
 
 	local system_path="$(cd $(dirname "${BASH_SOURCE[0]}")/../../.. && pwd)"
 
@@ -9,12 +9,16 @@ function compute_nix_path() {
 	local home_manager_store_path="$(nix-store -r $( nix-instantiate "${system_path}/external/home-manager.nix" 2>/dev/null ) 2>/dev/null)"
 
 	# change the NIX_PATH to point to what we need in order to run nixos-rebuild
-	nix_path="home-manager=${home_manager_store_path}"
-	nix_path="${nix_path}:nixos-config=${system_path}/nixos/machines/${machine}/configuration.nix"
-	nix_path="${nix_path}:nixos-hardware=${nixos_hardware_store_path}"
-	nix_path="${nix_path}:nixpkgs-overlays=${system_path}/overlays"
-	nix_path="${nix_path}:nixpkgs=${nixpkgs_store_path}"
-	nix_path="${nix_path}:system-path=${system_path}"
+	new_nix_path="home-manager=${home_manager_store_path}"
+	new_nix_path="${new_nix_path}:nixpkgs-overlays=${system_path}/overlays"
+	new_nix_path="${new_nix_path}:nixpkgs=${nixpkgs_store_path}"
+	new_nix_path="${new_nix_path}:system-path=${system_path}"
 
-	echo $nix_path
+	# add the nixos-config to the new_nix_path if /etc/NIXOS is found
+	if [[ -f /etc/NIXOS ]]; then
+		new_nix_path="${new_nix_path}:nixos-config=${system_path}/nixos/machines/${machine}/configuration.nix"
+		new_nix_path="${new_nix_path}:nixos-hardware=${nixos_hardware_store_path}"
+	fi
+
+	echo "${new_nix_path}"
 }
