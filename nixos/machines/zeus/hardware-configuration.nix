4ce5670ce841a05f75cf9a1c71107c36414338df
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-09-26 20:29:34 -0700

    add required kernel module

--- nixos/machines/zeus/hardware-configuration.nix
+++ nixos/machines/zeus/hardware-configuration.nix
@@ -13,7 +13,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "e1000e"];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
