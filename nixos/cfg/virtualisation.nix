c079ed3a79bf9810d4e087c7d698c8ac5912a63f
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-10-04 10:14:39 -0700

    nixos/cfg: virtualisation: move virtualbox option from common.nix to virtualisation.nix

--- nixos/cfg/virtualisation.nix
+++ nixos/cfg/virtualisation.nix
@@ -6,4 +6,7 @@
 
   # Enable Virtualbox support
   virtualisation.virtualbox.host.enable = true;
+
+  # install virtualbox extensions pack
+  nixpkgs.config.virtualbox.enableExtensionPack = true;
 }
