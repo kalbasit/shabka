fc18e30e9bd1351dd1aa0a522b9922e6b3c240c7
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-10-07 19:54:32 -0700

    bump the stateVersion

--- hosts/hades/configuration.nix
+++ hosts/hades/configuration.nix
@@ -16,7 +16,7 @@
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "18.03"; # Did you read the comment?
+  system.stateVersion = "18.09"; # Did you read the comment?
 }
 
 # {
