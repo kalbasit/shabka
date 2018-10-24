282c80bd6c32c73f8999dc75907a7cadcaf6db6e
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-10-24 02:00:40 -0700

    craete all users

--- modules/nixos/general/users.nix
+++ modules/nixos/general/users.nix
@@ -1,6 +1,34 @@
 { pkgs, ... }:
 
-{
+let
+  sshKeys = [
+    (builtins.readFile (builtins.fetchurl {
+      url = "https://github.com/kalbasit.keys";
+      sha256 = "033rs0pnm8aiycrfmx04qx8fmnkfdhp4hy3kdpgil3cgbgff9736";
+    }))
+  ];
+
+  makeUser = uid: admin: {
+    inherit uid;
+
+    group = "mine";
+    extraGroups = [
+      "docker"
+      "fuse"
+      "libvirtd"
+      "networkmanager"
+      "vboxusers"
+      "video"
+    ] ++ (if admin then ["wheel"] else []);
+
+    shell = pkgs.zsh;
+    hashedPassword = "$6$0bx5eAEsHJRxkD8.$gJ7sdkOOJRf4QCHWLGDUtAmjHV/gJxPQpyCEtHubWocHh9O7pWy10Frkm1Ch8P0/m8UTUg.Oxp.MB3YSQxFXu1";
+    isNormalUser = true;
+
+    openssh.authorizedKeys.keys = sshKeys;
+  };
+
+in {
   # set the initial password of the root user
   security.initialRootPassword = "$6$0bx5eAEsHJRxkD8.$gJ7sdkOOJRf4QCHWLGDUtAmjHV/gJxPQpyCEtHubWocHh9O7pWy10Frkm1Ch8P0/m8UTUg.Oxp.MB3YSQxFXu1";
 
@@ -14,40 +42,12 @@
     };
 
     users = {
-      kalbasit = {
-        uid = 2000;
+      yl = makeUser 2000 true;
+      yl_admin = makeUser 2001 false;
+      yl_opensource = makeUser 2003 false;
+      yl_publica = makeUser 2002 false;
 
-        group = "mine";
-        extraGroups = [
-          "docker"
-          "fuse"
-          "libvirtd"
-          "networkmanager"
-          "vboxusers"
-          "video"
-          "wheel"
-        ];
-
-        shell = pkgs.zsh;
-        hashedPassword = "$6$0bx5eAEsHJRxkD8.$gJ7sdkOOJRf4QCHWLGDUtAmjHV/gJxPQpyCEtHubWocHh9O7pWy10Frkm1Ch8P0/m8UTUg.Oxp.MB3YSQxFXu1";
-        isNormalUser = true;
-
-        openssh.authorizedKeys.keys = [
-          (builtins.readFile (builtins.fetchurl {
-            url = "https://github.com/kalbasit.keys";
-            sha256 = "033rs0pnm8aiycrfmx04qx8fmnkfdhp4hy3kdpgil3cgbgff9736";
-          }))
-        ];
-      };
-
-      root = {
-        openssh.authorizedKeys.keys = [
-          (builtins.readFile (builtins.fetchurl {
-            url = "https://github.com/kalbasit.keys";
-            sha256 = "033rs0pnm8aiycrfmx04qx8fmnkfdhp4hy3kdpgil3cgbgff9736";
-          }))
-        ];
-      };
+      root = { openssh.authorizedKeys.keys = sshKeys; };
     };
   };
 }
