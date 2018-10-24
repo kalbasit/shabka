3da18a1b55345a554b366a88bb76f3107b193e8b
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-10-24 15:35:03 -0700

    improve the documentation

--- modules/home/generic/nixos.nix
+++ modules/home/generic/nixos.nix
@@ -6,7 +6,8 @@ with lib;
   options.mine.nixosConfig = mkOption {
     default = {};
     defaultText = ''
-      NixOS configuration, set only on NixOS machines
+      NixOS configuration. On NixOS machines, it should be the config itself.
+      On non-NixOS machines, all the required keys must be set manually.
     '';
   };
 }
