16736f0347c7aa8f5b9c9688a655a82077d3a2bc
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-10-26 20:44:04 -0700

    hosts/hades: refactor the configuration to generate the final fileSystems

--- hosts/hades/hardware-configuration.nix
+++ hosts/hades/hardware-configuration.nix
@@ -1,112 +1,67 @@
-{
-  imports = [
-    <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-  ];
+{ lib, ... }:
+
+with lib;
+
+let
+  rootDevice   = "/dev/disk/by-uuid/e2b2367b-f458-4c55-a3f0-87cef3366d62";
+  bootDevice   = "/dev/disk/by-uuid/F4E5-ABC1";
+  swapDevice   = "/dev/disk/by-uuid/a3e2591f-a1d4-4d75-b09c-094416b485c4";
+  storgeDevice = "/dev/disk/by-uuid/d8a3aad7-3fe8-4986-acc5-c6f7525c9af4";
+
+  subVolumes =
+    {
+      # NixOS
+      "/"                      = { device = rootDevice;   subvol = "@nixos/@root"; };
+      "/home"                  = { device = rootDevice;   subvol = "@nixos/@home"; };
+      "/home/kalbasit/code"    = { device = rootDevice;   subvol = "@code"; options = [ "X-mount.mkdir=0700" ]; };
+      "/home/kalbasit/private" = { device = rootDevice;   subvol = "@private"; options = [ "X-mount.mkdir=0700" ]; };
+      "/home/kalbasit/storage" = { device = storgeDevice; subvol = "@home-kalbasit-storage"; };
+
+      # ArchOS
+
+      "/mnt/arch"                       = { device = rootDevice;   subvol = "@arch/@root"; };
+      "/mnt/arch/home"                  = { device = rootDevice;   subvol = "@arch/@home"; };
+      "/mnt/arch/home/kalbasit/code"    = { device = rootDevice;   subvol = "@code"; };
+      "/mnt/arch/home/kalbasit/storage" = { device = storgeDevice; subvol = "@home-kalbasit-storage"; };
+    };
+
+  mkBtrfsSubvolume = mountPoint: { device, subvol, options ? [] }:
+    nameValuePair
+      (mountPoint)
+      ({
+        inherit device;
+        fsType = "btrfs";
+        options =
+          [ "subvol=${subvol}" ]
+          ++ options;
+      });
+
+in {
+  imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
 
   boot.initrd.luks.devices = {
-    cryptkey = {
-      device = "/dev/disk/by-uuid/3830842b-a589-45e0-a51d-4ed516472575";
-    };
-
-    cryptroot = {
-      device = "/dev/disk/by-uuid/ec133595-6294-4756-95ea-3891297f6477";
-      keyFile = "/dev/mapper/cryptkey";
-    };
-
-    cryptswap = {
-      device = "/dev/disk/by-uuid/71aab354-3872-4316-a26c-b4090d73275c";
-      keyFile = "/dev/mapper/cryptkey";
-    };
-
-    cryptstorage = {
-      device = "/dev/disk/by-uuid/d339f141-7544-44d4-89ec-312af0e087cc";
-      keyFile = "/dev/mapper/cryptkey";
-    };
+    cryptkey     = { device = "/dev/disk/by-uuid/3830842b-a589-45e0-a51d-4ed516472575"; };
+    cryptroot    = { device = "/dev/disk/by-uuid/ec133595-6294-4756-95ea-3891297f6477"; keyFile = "/dev/mapper/cryptkey"; };
+    cryptswap    = { device = "/dev/disk/by-uuid/71aab354-3872-4316-a26c-b4090d73275c"; keyFile = "/dev/mapper/cryptkey"; };
+    cryptstorage = { device = "/dev/disk/by-uuid/d339f141-7544-44d4-89ec-312af0e087cc"; keyFile = "/dev/mapper/cryptkey"; };
   };
 
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-uuid/e2b2367b-f458-4c55-a3f0-87cef3366d62";
-      fsType = "btrfs";
-      options = [ "subvol=@nixos/@root" ];
+  fileSystems = mergeAttrs
+    (mapAttrs' mkBtrfsSubvolume subVolumes)
+    {
+      # NixOS
+
+      "/boot" = { device = bootDevice; fsType = "vfat"; };
+      "/mnt/volumes/root" = { device = rootDevice; fsType = "btrfs"; };
+
+      # Storage
+
+      "/mnt/volumes/storage" = { device = storgeDevice; fsType = "btrfs"; };
+
+      # ArchOS
+
+      "/mnt/arch/boot/efi" = { device = bootDevice; fsType = "vfat"; };
     };
 
-    "/home" = {
-      device = "/dev/disk/by-uuid/e2b2367b-f458-4c55-a3f0-87cef3366d62";
-      fsType = "btrfs";
-      options = [ "subvol=@nixos/@home" ];
-    };
-
-    "/home/kalbasit/code" = {
-      device = "/dev/disk/by-uuid/e2b2367b-f458-4c55-a3f0-87cef3366d62";
-      fsType = "btrfs";
-      options = [ "subvol=@code" "X-mount.mkdir=0700" ];
-    };
-
-    "/home/kalbasit/private" = {
-      device = "/dev/disk/by-uuid/e2b2367b-f458-4c55-a3f0-87cef3366d62";
-      fsType = "btrfs";
-      options = [ "subvol=@private" "X-mount.mkdir=0700" ];
-    };
-
-    "/home/kalbasit/storage" = {
-      device = "/dev/disk/by-uuid/d8a3aad7-3fe8-4986-acc5-c6f7525c9af4";
-      fsType = "btrfs";
-      options = [ "subvol=@home-kalbasit-storage" ];
-    };
-
-    "/boot" = {
-      device = "/dev/disk/by-uuid/F4E5-ABC1";
-      fsType = "vfat";
-    };
-
-    # Root
-
-    "/mnt/volumes/root" = {
-      device = "/dev/disk/by-uuid/e2b2367b-f458-4c55-a3f0-87cef3366d62";
-      fsType = "btrfs";
-    };
-
-    # Storage
-
-    "/mnt/volumes/storage" = {
-      device = "/dev/disk/by-uuid/d8a3aad7-3fe8-4986-acc5-c6f7525c9af4";
-      fsType = "btrfs";
-    };
-
-    # ArchOS
-
-    "/mnt/arch" = {
-      device = "/dev/disk/by-uuid/e2b2367b-f458-4c55-a3f0-87cef3366d62";
-      fsType = "btrfs";
-      options = [ "subvol=@arch/@root" ];
-    };
-
-    "/mnt/arch/home" = {
-      device = "/dev/disk/by-uuid/e2b2367b-f458-4c55-a3f0-87cef3366d62";
-      fsType = "btrfs";
-      options = [ "subvol=@arch/@home" ];
-    };
-
-    "/mnt/arch/home/kalbasit/code" = {
-      device = "/dev/disk/by-uuid/e2b2367b-f458-4c55-a3f0-87cef3366d62";
-      fsType = "btrfs";
-      options = [ "subvol=@code" ];
-    };
-
-    "/mnt/arch/home/kalbasit/storage" = {
-      device = "/dev/disk/by-uuid/d8a3aad7-3fe8-4986-acc5-c6f7525c9af4";
-      fsType = "btrfs";
-      options = [ "subvol=@home-kalbasit-storage" ];
-    };
-
-    "/mnt/arch/boot/efi" = {
-      device = "/dev/disk/by-uuid/F4E5-ABC1";
-      fsType = "vfat";
-    };
-  };
-
-  swapDevices = [
-    { device = "/dev/disk/by-uuid/a3e2591f-a1d4-4d75-b09c-094416b485c4"; }
-  ];
+  swapDevices = [ { device = swapDevice; } ];
 }
