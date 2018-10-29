dd9d4df3319b7034b70b04360121f8c954fefc97
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-10-29 10:58:12 -0700

    home: workstation: write down mimeapps.list and set the default browser to rbrowser

--- modules/home/workstation/default.nix
+++ modules/home/workstation/default.nix
@@ -13,25 +13,51 @@ with import ../../../util;
     services.keybase.enable = true;
     services.network-manager-applet.enable = true;
 
-    home.packages = with pkgs; [
-      keybase-gui
+    home = {
+      packages = with pkgs; [
+        keybase-gui
 
-      nur.repos.kalbasit.rbrowser
+        nur.repos.kalbasit.rbrowser
 
-      remmina
+        remmina
 
-      weechat
+        weechat
 
-      xsel
+        xsel
 
-      # zoom for meetings
-      zoom-us
-    ];
+        # zoom for meetings
+        zoom-us
+      ];
 
-    home.activation = {
-      rbrowser-desktop-link = symlink
+      activation = {
+        rbrowser-desktop-link = symlink
         "${pkgs.nur.repos.kalbasit.rbrowser}/share/applications/rbrowser.desktop"
         "${config.home.homeDirectory}/.local/share/applications/rbrowser.desktop";
+      };
+
+      file = {
+        ".local/share/applications/mimeapps.list".text = let
+          mimeTypes =
+            [
+              "application/pdf"
+              "application/xhtml+xml"
+              "text/html"
+              "text/xml"
+              "x-scheme-handler/about"
+              "x-scheme-handler/http"
+              "x-scheme-handler/https"
+              "x-scheme-handler/unknown"
+            ];
+
+          rbrowser = builtins.concatStringsSep
+            "\n"
+            (map (typ: "${typ}=rbrowser.desktop") mimeTypes);
+
+        in ''
+          [Default Applications]
+          ${rbrowser}
+        '';
+      };
     };
 
     programs.zsh.initExtra = ''
