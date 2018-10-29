8880a602d400bf61cbbc44b4b0570cdf730e8d1c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-10-28 17:58:44 -0700

    nixos: workstation: install virtualbox extension pack

--- modules/nixos/workstation/virtualbox.nix
+++ modules/nixos/workstation/virtualbox.nix
@@ -6,11 +6,7 @@ with lib;
   options.mine.workstation.virtualbox.enable = mkEnableOption "Enable VirtualBox";
 
   config = mkIf config.mine.workstation.virtualbox.enable {
-    # Enable Virtualbox support
     virtualisation.virtualbox.host.enable = true;
-
-    # install virtualbox extensions pack
-    nixpkgs.config.virtualbox.enableExtensionPack = true;
+    virtualisation.virtualbox.host.enableExtensionPack = true;
   };
 }
-
