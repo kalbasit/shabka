5cbb5d82712e420ba57049c3104f834e0a392290
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-10-29 17:24:02 -0700

    nixos: move a zsh setting from the editor to zsh expression

--- modules/nixos/general/editor.nix
+++ modules/nixos/general/editor.nix
@@ -1,9 +1,6 @@
 { pkgs, ... }:
 
 {
-  # install all completions libraries for system packages
-  environment.pathsToLink = [ "/share/zsh" ];
-
   # set the EDITOR to neovim
   environment.variables.EDITOR = "nvim";
 
--- modules/nixos/software/zsh.nix
+++ modules/nixos/software/zsh.nix
@@ -1,4 +1,7 @@
 {
+  # install all completions libraries for system packages
+  environment.pathsToLink = [ "/share/zsh" ];
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
