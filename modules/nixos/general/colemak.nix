f33a1abd9a9203af68db6797ce1d0fa66deddf84
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-10-31 14:08:35 -0700

    nixos: add a note about colemak mapping

--- modules/nixos/general/colemak.nix
+++ modules/nixos/general/colemak.nix
@@ -7,6 +7,9 @@ with lib;
 
   config = mkIf config.mine.useColemakKeyboardLayout {
     boot.earlyVconsoleSetup = true;
+    # In the following commit, the colemak/en-latin9 keyboard mapping was
+    # renamed to colemak.
+    # https://github.com/NixOS/nixpkgs/commit/f1987fb58f57828944ca822bbb39b3de87f01863
     i18n.consoleKeyMap = "colemak/en-latin9";
     services.xserver.layout = "us";
     services.xserver.xkbVariant = "colemak";
