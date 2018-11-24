c3ec6490a89f1730ca100b98a7148effe56664c3
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-11-23 18:17:20 -0800

    astroid: poll script should invoke offlineimap

--- modules/home/workstation/email.nix
+++ modules/home/workstation/email.nix
@@ -59,6 +59,10 @@ assert assertMsg (builtins.pathExists privateEmailPath) "Private email configura
       externalEditor = ''
         ${getBin pkgs.alacritty}/bin/alacritty --title email --command nvim -c 'set ft=mail' '+set fileencoding=utf-8' '+set ff=unix' '+set enc=utf-8' '+set fo+=w' %1
       '';
+
+      pollScript = ''
+        offlineimap -o >> ${config.xdg.dataHome}/offlineimap/poll.log 2>&1
+      '';
     };
 
     programs.msmtp.enable = true;
