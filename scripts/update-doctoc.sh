a6b1017ea6591a18653cf5f32adf40bb2e223738
Wael Nasreddine <wael.nasreddine@gmail.com>
2018-11-26 12:20:28 -0800

    README: extract the documentation to doc/modules.md (#60)

--- scripts/update-doctoc.sh
+++ scripts/update-doctoc.sh
@@ -5,3 +5,4 @@ readonly here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${here}/.."
 
 find . -name README.md -exec ./libexec/doctoc/wrapper.sh --github --title "**Table of Contents**" {} \;
+find doc -name '*.md' -exec ./libexec/doctoc/wrapper.sh --github --title "**Table of Contents**" {} \;
