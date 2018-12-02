f4dd66f419ee571793c8d23b0429e15e066a5ff0
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2018-12-01 23:33:09 -0800

    get rid of home-manager channel

--- util/buildHomeManagerConfiguration.nix
+++ util/buildHomeManagerConfiguration.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ pkgs, lib }:
 
-{
-  buildHomeManagerConfiguration = conf: (import <home-manager/home-manager/home-manager.nix> {
+let
+  homeManager = import ../external/home-manager.nix {
+    inherit pkgs;
+    inherit (import ./assertMsg.nix { inherit lib; }) assertMsg;
+  };
+
+in {
+  buildHomeManagerConfiguration = conf: (import "${homeManager}/home-manager/home-manager.nix" {
     confPath = conf;
     confAttr = "";
   }).activationPackage;
