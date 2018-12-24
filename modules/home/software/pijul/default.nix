35e27095411c03af29e7ff94a29b133c9df3c7d4
Wael Nasreddine <wael.nasreddine@gmail.com>
2018-12-23 22:37:54 -0800

    home: install pijul from unstable (#71)

---
+++ modules/home/software/pijul/default.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+{
+  options.mine.pijul.enable = mkEnableOption "pijul";
+
+  config = mkIf config.mine.pijul.enable {
+    home.packages = with pkgs; [
+      unstable.pijul
+    ];
+  };
+}
