be4ecfb797ecde423330a708983b4f439c3f600a
Wael Nasreddine <wael.nasreddine@gmail.com>
2019-01-10 20:45:22 -0800

    home: i3: put Keybase gui in a scratchpad (#100)

--- modules/home/software/keybase.nix
+++ modules/home/software/keybase.nix
@@ -8,7 +8,5 @@ with lib;
   config = mkIf config.mine.keybase.enable {
     services.kbfs.enable = true;
     services.keybase.enable = true;
-
-    home.packages = optionals config.mine.workstation.enable [ pkgs.keybase-gui ];
   };
 }
