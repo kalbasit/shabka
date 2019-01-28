04484a82366a81a853c382dadced83db435c7c40
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-01-27 18:09:59 -0800

    remove the patch not wroking

--- modules/home/workstation/dropbox.nix
+++ modules/home/workstation/dropbox.nix
@@ -2,27 +2,7 @@
 
 with lib;
 
-let
-
-  fakeExt4 = pkgs.stdenv.mkDerivation rec {
-    name = "fakext4-${version}";
-    version = "0.0.1";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "dark";
-      repo = "dropbox-filesystem-fix";
-      rev = "d284f5d4884003c2dad24eac88b5e285f6281624";
-      sha256 = "0a5gfb11nb26lpavppyfifklnw515sg402sy9cqm8h39gw2zkb87";
-    };
-
-    buildInputs = [ pkgs.makeWrapper ];
-
-    installPhase = ''
-      install -Dm644 libdropbox_fs_fix.so $out/lib/libdropbox_fs_fix.so
-    '';
-  };
-
-in {
+{
   options.mine.workstation.dropbox.enable = mkEnableOption "Enable Dropbox";
 
   config = mkIf config.mine.workstation.dropbox.enable {
@@ -42,7 +22,6 @@ in {
         ProtectSystem = "full";
         Nice = 10;
         environment = [
-          "LD_PRELOAD=${getLib fakeExt4}/lib/libdropbox_fs_fix.so"
           "QML2_IMPORT_PATH=/run/current-system/sw/${pkgs.qt5.qtbase.qtQmlPrefix}"
           "QT_PLUGIN_PATH=/run/current-system/sw/${pkgs.qt5.qtbase.qtPluginPrefix}"
         ];
