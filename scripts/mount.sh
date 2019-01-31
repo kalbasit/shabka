09df2a7c3c73a32681208a62e37c5bf7a39dbe5c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-01-30 23:08:14 -0800

    allow swap to fail

--- scripts/mount.sh
+++ scripts/mount.sh
@@ -5,7 +5,10 @@ set -euo pipefail
 # TODO: This must be able to mount any host by parsing directly the hardware
 # configuration
 
+# NOTE: We allow swap to error out
+set +e
 swapon /dev/mapper/cryptswap
+set +e
 
 mount -o subvol=@nixos/@root /mnt
 mount -o subvol=@nixos/@home /mnt/home
