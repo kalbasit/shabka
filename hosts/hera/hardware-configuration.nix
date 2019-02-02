a998648e3589e6eaf5395609907f102c75cf41a3
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-02-02 13:24:55 -0800

    install new root TODO: storaage

--- hosts/hera/hardware-configuration.nix
+++ hosts/hera/hardware-configuration.nix
@@ -3,7 +3,7 @@
 with lib;
 
 let
-  rootDevice   = "/dev/disk/by-uuid/015a0fac-a78b-45d9-90b5-eb1bd369c241";
+  rootDevice   = "/dev/disk/by-uuid/1da7cc56-a28d-4333-ab70-7a595f7df47c";
   bootDevice   = "/dev/disk/by-uuid/ADE1-3154";
   swapDevice   = "/dev/disk/by-uuid/a54fc702-d678-4bb1-9448-940f2337cefe";
 
@@ -32,8 +32,8 @@ in {
   imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
 
   boot.initrd.luks.devices = {
-    cryptkey     = { device = "/dev/disk/by-uuid/339cc5ba-1a34-428d-bc9b-b5e10b14eaf0"; };
-    cryptroot    = { device = "/dev/disk/by-uuid/15d91894-a9dc-4288-9589-bcb7945d02ce"; keyFile = "/dev/mapper/cryptkey"; };
+    cryptkey     = { device = "/dev/disk/by-uuid/9e7505bf-4be5-47aa-8225-bed182cf58f2"; };
+    cryptroot    = { device = "/dev/disk/by-uuid/f9dff108-7369-4dbe-9fe5-46e795fd7b25"; keyFile = "/dev/mapper/cryptkey"; };
     cryptswap    = { device = "/dev/disk/by-uuid/7cd0fad9-4af1-40af-9576-af9c6212d9b9"; keyFile = "/dev/mapper/cryptkey"; };
   };
 
