db4963bc6ea1861987de8480010e564e858b2790
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-02-02 14:51:27 -0800

    create an easy make target to do that

--- external/kalbasit-keys.nix
+++ external/kalbasit-keys.nix
@@ -1,7 +1,7 @@
 let
+  keysVersion = builtins.fromJSON (builtins.readFile ./kalbasit-keys-version.json);
   keys = builtins.fetchurl {
-    url = "https://github.com/kalbasit.keys";
-    sha256 = "1ka0wk5gb82hdfmz575c78j6pyqqshj4xqm37zizd1pxrl0fsifw";
+    inherit (keysVersion) url sha256;
   };
 in
   keys
