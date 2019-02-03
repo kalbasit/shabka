8529287a50f28eb1b90384466d5b4fc3b7866bd6
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-02-02 18:34:41 -0800

    nixos: GnuPG: enable support for SSH

--- modules/nixos/services/gnupg.nix
+++ modules/nixos/services/gnupg.nix
@@ -6,7 +6,10 @@ with lib;
   options.mine.gnupg.enable = mkEnableOption "Enable GnuPG";
 
   config = mkIf config.mine.gnupg.enable {
-    programs.gnupg.agent.enable = true;
-    programs.gnupg.agent.enableBrowserSocket = true;
-  };
+    programs.gnupg.agent = {
+      enable = true;
+      enableBrowserSocket = true;
+      enableSSHSupport = true;
+    };
+   };
 }
