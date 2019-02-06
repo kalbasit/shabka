77f5dda276a13e8469e8de7e5c41660121ef6465
Wael Nasreddine <wael.nasreddine@gmail.com>
2019-02-05 21:59:22 -0800

    hosts/cratos: use the xps-9380 nixos hardware profile (#140)
    I was getting black screens and random lockups when using the XPS 9370 profile. It turned out to be due to the changes specific to Kaby Lake that are being imported. This CPU is actually Whiskey Lake. The profile also disables a bug fix that is not present on this model.
    
    See https://github.com/NixOS/nixos-hardware/pull/97
    
    fixes #138
    
    This reverts commit 6f1d472a0cbdaa1bc4d182ec030865d534e42a0e

--- external/nixos-hardware.nix
+++ external/nixos-hardware.nix
@@ -1,7 +1,34 @@
+{ fetchpatch, runCommand }:
+
 let
   pinnedVersion = builtins.fromJSON (builtins.readFile ./nixos-hardware-version.json);
   pinned = builtins.fetchTarball {
     inherit (pinnedVersion) url sha256;
   };
+
+  patches = [
+    # Dell XPS 9380 hardware profile
+    # https://github.com/NixOS/nixos-hardware/pull/97
+    (fetchpatch {
+      url = "https://github.com/NixOS/nixos-hardware/pull/97.patch";
+      sha256 = "1pryw8kwk8h99ag08wni6gmkyrpabgyzm55lf8b6zd25ph4027m5";
+    })
+  ];
+
+  patched = runCommand "nixos-hardware-${pinnedVersion.rev}"
+    {
+      inherit pinned patches;
+
+      preferLocalBuild = true;
+    }
+    ''
+      cp -r $pinned $out
+      chmod -R +w $out
+      for p in $patches; do
+        echo "Applying patch $p";
+        patch -d $out -p1 < "$p";
+      done
+    '';
+
 in
-  pinned
+  patched
