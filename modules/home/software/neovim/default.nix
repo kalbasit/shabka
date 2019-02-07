3f729a35a4fd4be561ecb8ff423fae0c79ae056d
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-02-06 21:46:45 -0800

    I moved the module, but I am seeing a difference when I diff the build

--- modules/home/software/neovim/default.nix
+++ modules/home/software/neovim/default.nix
@@ -32,97 +32,20 @@ in {
         Extra NeoVim plugin dictionary.
       '';
     };
+
+    keyboardLayout = mkOption {
+      type = with types; enum [ "colemak" "qwerty" ];
+      default = /*if config.mine.useColemakKeyboardLayout then*/ "colemak" /* else "qwerty"*/;
+      description = ''
+        The keyboard layout to use.
+      '';
+    };
   };
 
   config = mkIf cfg.enable {
-    programs.neovim = {
-      enable = true;
-
-      viAlias = true;
-      vimAlias = true;
-
-      withPython = true;
-      extraPythonPackages = ps: with ps; [ pynvim ];
-
-      withPython3 = true;
-      extraPython3Packages = ps: with ps; [ pynvim ];
-
-      configure = {
-        customRC = cfg.extraRC + (builtins.readFile (pkgs.substituteAll {
-          src = ./init.vim;
-
-          ag_bin = "${pkgs.ag}/bin/ag";
-          gocode_bin = "${pkgs.nur.repos.kalbasit.gocode}/bin/gocode";
-          xsel_bin = "${pkgs.xsel}/bin/xsel";
-        })) + (if config.mine.useColemakKeyboardLayout then (builtins.readFile ./colemak.vim) else "");
-
-        vam.knownPlugins = pkgs.vimPlugins // cfg.extraKnownPlugins;
-        vam.pluginDictionaries = cfg.extraPluginDictionaries ++ [
-          {
-            names =
-              [
-                "Gist"
-                "Gundo"
-                "LanguageClient-neovim"
-                "PreserveNoEOL"
-                "ack-vim"
-                "ale"
-                "auto-pairs"
-                "caw"
-                "csv-vim"
-                "direnv-vim"
-                "easy-align"
-                "easymotion"
-                "editorconfig-vim"
-                "emmet-vim"
-                "fzf-vim"
-                "fzfWrapper"
-                "goyo"
-                "multiple-cursors"
-                "ncm2"
-                "pig-vim"
-                "repeat"
-                "rhubarb"
-                "sleuth"
-                "surround"
-                "traces-vim"
-                "vim-airline"
-                "vim-airline-themes"
-                "vim-better-whitespace"
-                "vim-eunuch"
-                "vim-fugitive"
-                "vim-markdown"
-                "vim-signify"
-                "vim-speeddating"
-                "vim-terraform"
-                "vimtex"
-                "vissort-vim"
-                "zoomwintab-vim"
-
-                # NOTE: Keep vim-go before PolyGlot. If PolyGlot is loaded first, vim-go will fail with the error `E117: Unknown function: go#config#VersionWarning`.
-                # See https://github.com/sheerun/vim-polyglot/issues/309
-                "vim-go"
-                "polyglot"
-
-                ## DeoPlete completion support
-                "deoplete-nvim"
-
-                # Golang support
-                "deoplete-go"
-
-                "vim-maktaba"
-                "vim-bazel"
-
-
-                # Typescript support
-                # "vim-typescript"    # TODO: https://github.com/kalbasit/dotfiles/issues/15
-                "yats-vim"
-
-              ]
-              ++ (if config.mine.useColemakKeyboardLayout then ["vim-colemak"] else []);
-          }
-        ];
-      };
+    programs.neovim = import ../../../neovim {
+      inherit (cfg) extraRC extraKnownPlugins extraPluginDictionaries keyboardLayout;
+      inherit pkgs;
     };
   };
 }
---
+++ modules/neovim/default.nix
@@ -0,0 +1,102 @@
+{ extraRC
+, extraKnownPlugins
+, extraPluginDictionaries
+, keyboardLayout
+, pkgs
+}:
+
+{
+  enable = true;
+
+  viAlias = true;
+  vimAlias = true;
+
+  withPython = true;
+  extraPythonPackages = ps: with ps; [ pynvim ];
+
+  withPython3 = true;
+  extraPython3Packages = ps: with ps; [ pynvim ];
+
+  configure = {
+    customRC = builtins.concatStringsSep " " [
+      (builtins.readFile (pkgs.substituteAll {
+        src = ./init.vim;
+
+        ag_bin = "${pkgs.ag}/bin/ag";
+        gocode_bin = "${pkgs.nur.repos.kalbasit.gocode}/bin/gocode";
+        xsel_bin = "${pkgs.xsel}/bin/xsel";
+      }))
+
+      extraRC
+
+      (builtins.readFile ./keyboard_layouts + "${keyboardLayout}.vim")
+    ];
+
+    vam.knownPlugins = pkgs.vimPlugins // extraKnownPlugins;
+    vam.pluginDictionaries = extraPluginDictionaries ++ [
+      {
+        names =
+          [
+            "Gist"
+            "Gundo"
+            "LanguageClient-neovim"
+            "PreserveNoEOL"
+            "ack-vim"
+            "ale"
+            "auto-pairs"
+            "caw"
+            "csv-vim"
+            "direnv-vim"
+            "easy-align"
+            "easymotion"
+            "editorconfig-vim"
+            "emmet-vim"
+            "fzf-vim"
+            "fzfWrapper"
+            "goyo"
+            "multiple-cursors"
+            "ncm2"
+            "pig-vim"
+            "repeat"
+            "rhubarb"
+            "sleuth"
+            "surround"
+            "traces-vim"
+            "vim-airline"
+            "vim-airline-themes"
+            "vim-better-whitespace"
+            "vim-eunuch"
+            "vim-fugitive"
+            "vim-markdown"
+            "vim-signify"
+            "vim-speeddating"
+            "vim-terraform"
+            "vimtex"
+            "vissort-vim"
+            "zoomwintab-vim"
+
+            # NOTE: Keep vim-go before PolyGlot. If PolyGlot is loaded first, vim-go will fail with the error `E117: Unknown function: go#config#VersionWarning`.
+            # See https://github.com/sheerun/vim-polyglot/issues/309
+            "vim-go"
+            "polyglot"
+
+            ## DeoPlete completion support
+            "deoplete-nvim"
+
+            # Golang support
+            "deoplete-go"
+
+            "vim-maktaba"
+            "vim-bazel"
+
+
+            # Typescript support
+            # "vim-typescript"    # TODO: https://github.com/kalbasit/dotfiles/issues/15
+            "yats-vim"
+
+          ]
+          ++ (if keyboardLayout == "colemak" then ["vim-colemak"] else []);
+      }
+    ];
+  };
+}
