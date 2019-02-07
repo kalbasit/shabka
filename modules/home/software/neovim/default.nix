48a0bde07892fc6bfeca9575ea5cf625cd3b4c99
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-02-06 22:16:28 -0800

    revert back to a regular function

--- modules/home/software/neovim/default.nix
+++ modules/home/software/neovim/default.nix
@@ -7,10 +7,6 @@ let
   cfg = config.mine.neovim;
 
 in {
-  imports = [
-    ../../../neovim
-  ];
-
   options.mine.neovim = {
     enable = mkEnableOption "neovim";
 
@@ -23,7 +19,6 @@ in {
     };
 
     extraKnownPlugins = mkOption {
-      type = types.attrs;
       default = {};
       description = ''
         Extra NeoVim known plugins.
@@ -39,33 +34,18 @@ in {
     };
 
     keyboardLayout = mkOption {
-      type = types.enum [ "colemak" "qwerty" ];
-      default = if config.mine.useColemakKeyboardLayout then "colemak" else "qwerty";
+      type = with types; enum [ "colemak" "qwerty" ];
+      default = /*if config.mine.useColemakKeyboardLayout then*/ "colemak" /* else "qwerty"*/;
       description = ''
         The keyboard layout to use.
       '';
     };
   };
 
-  config = {
-    programs.neovim = mkIf cfg.enable {
-      config.shabka.neovim = {
-        inherit (cfg) enable extraRC extraKnownPlugins extraPluginDictionaries keyboardLayout;
-      };
-
-      programs.neovim = {
-
-      };
-    }
-      mkMerge [
-      (import ../../../neovim {
-        inherit lib pkgs;
-
-        config.shabka.neovim = {
-        };
-      }).config
-
-      { inherit (cfg) enable; }
-    ];
+  config = mkIf cfg.enable {
+    programs.neovim = import ../../../neovim {
+      inherit (cfg) extraRC extraKnownPlugins extraPluginDictionaries keyboardLayout;
+      inherit pkgs;
+    };
   };
 }
--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -1,139 +1,102 @@
-{ config, lib, pkgs, ... }:
+{ extraRC
+, extraKnownPlugins
+, extraPluginDictionaries
+, keyboardLayout
+, pkgs
+}:
 
-with lib;
+{
+  enable = true;
 
-let
+  viAlias = true;
+  vimAlias = true;
 
-  cfg = config.shabka.neovim;
+  withPython = true;
+  extraPythonPackages = ps: with ps; [ pynvim ];
 
-in {
-  options.shabka.neovim = {
-    enable = mkEnableOption "neovim";
+  withPython3 = true;
+  extraPython3Packages = ps: with ps; [ pynvim ];
 
-    extraRC = mkOption {
-      type = types.str;
-      default = "";
-      description = ''
-        Extra NeoVim init configuration.
-      '';
-    };
+  configure = {
+    customRC = builtins.concatStringsSep " " [
+      (builtins.readFile (pkgs.substituteAll {
+        src = ./init.vim;
 
-    extraKnownPlugins = mkOption {
-      type = types.attrs;
-      default = {};
-      description = ''
-        Extra NeoVim known plugins.
-      '';
-    };
+        ag_bin = "${pkgs.ag}/bin/ag";
+        gocode_bin = "${pkgs.nur.repos.kalbasit.gocode}/bin/gocode";
+        xsel_bin = "${pkgs.xsel}/bin/xsel";
+      }))
 
-    extraPluginDictionaries = mkOption {
-      type = with types; listOf attrs;
-      default = [];
-      description = ''
-        Extra NeoVim plugin dictionary.
-      '';
-    };
+      extraRC
 
-    keyboardLayout = mkOption {
-      type = types.enum [ "colemak" "qwerty" ];
-      default = "qwerty";
-      description = ''
-        The keyboard layout to use.
-      '';
-    };
-  };
+      (builtins.readFile ./keyboard_layouts + "${keyboardLayout}.vim")
+    ];
 
-  config = mkIf cfg.enable {
-    viAlias = true;
-    vimAlias = true;
+    vam.knownPlugins = pkgs.vimPlugins // extraKnownPlugins;
+    vam.pluginDictionaries = extraPluginDictionaries ++ [
+      {
+        names =
+          [
+            "Gist"
+            "Gundo"
+            "LanguageClient-neovim"
+            "PreserveNoEOL"
+            "ack-vim"
+            "ale"
+            "auto-pairs"
+            "caw"
+            "csv-vim"
+            "direnv-vim"
+            "easy-align"
+            "easymotion"
+            "editorconfig-vim"
+            "emmet-vim"
+            "fzf-vim"
+            "fzfWrapper"
+            "goyo"
+            "multiple-cursors"
+            "ncm2"
+            "pig-vim"
+            "repeat"
+            "rhubarb"
+            "sleuth"
+            "surround"
+            "traces-vim"
+            "vim-airline"
+            "vim-airline-themes"
+            "vim-better-whitespace"
+            "vim-eunuch"
+            "vim-fugitive"
+            "vim-markdown"
+            "vim-signify"
+            "vim-speeddating"
+            "vim-terraform"
+            "vimtex"
+            "vissort-vim"
+            "zoomwintab-vim"
 
-    withPython = true;
-    extraPythonPackages = ps: with ps; [ pynvim ];
+            # NOTE: Keep vim-go before PolyGlot. If PolyGlot is loaded first, vim-go will fail with the error `E117: Unknown function: go#config#VersionWarning`.
+            # See https://github.com/sheerun/vim-polyglot/issues/309
+            "vim-go"
+            "polyglot"
 
-    withPython3 = true;
-    extraPython3Packages = ps: with ps; [ pynvim ];
+            ## DeoPlete completion support
+            "deoplete-nvim"
 
-    configure = {
-      customRC = builtins.concatStringsSep " " [
-        (builtins.readFile (pkgs.substituteAll {
-          src = ./init.vim;
+            # Golang support
+            "deoplete-go"
 
-          ag_bin = "${pkgs.ag}/bin/ag";
-          gocode_bin = "${pkgs.nur.repos.kalbasit.gocode}/bin/gocode";
-          xsel_bin = "${pkgs.xsel}/bin/xsel";
-        }))
-
-        cfg.extraRC
-
-        (builtins.readFile ./keyboard_layouts + "${cfg.keyboardLayout}.vim")
-      ];
-
-      vam.knownPlugins = pkgs.vimPlugins // cfg.extraKnownPlugins;
-      vam.pluginDictionaries = cfg.extraPluginDictionaries ++ [
-        {
-          names =
-            [
-              "Gist"
-              "Gundo"
-              "LanguageClient-neovim"
-              "PreserveNoEOL"
-              "ack-vim"
-              "ale"
-              "auto-pairs"
-              "caw"
-              "csv-vim"
-              "direnv-vim"
-              "easy-align"
-              "easymotion"
-              "editorconfig-vim"
-              "emmet-vim"
-              "fzf-vim"
-              "fzfWrapper"
-              "goyo"
-              "multiple-cursors"
-              "ncm2"
-              "pig-vim"
-              "repeat"
-              "rhubarb"
-              "sleuth"
-              "surround"
-              "traces-vim"
-              "vim-airline"
-              "vim-airline-themes"
-              "vim-better-whitespace"
-              "vim-eunuch"
-              "vim-fugitive"
-              "vim-markdown"
-              "vim-signify"
-              "vim-speeddating"
-              "vim-terraform"
-              "vimtex"
-              "vissort-vim"
-              "zoomwintab-vim"
-
-              # NOTE: Keep vim-go before PolyGlot. If PolyGlot is loaded first, vim-go will fail with the error `E117: Unknown function: go#config#VersionWarning`.
-              # See https://github.com/sheerun/vim-polyglot/issues/309
-              "vim-go"
-              "polyglot"
-
-              ## DeoPlete completion support
-              "deoplete-nvim"
-
-              # Golang support
-              "deoplete-go"
-
-              "vim-maktaba"
-              "vim-bazel"
+            "vim-maktaba"
+            "vim-bazel"
 
 
-              # Typescript support
-              # "vim-typescript"    # TODO: https://github.com/kalbasit/dotfiles/issues/15
-              "yats-vim"
+            # Typescript support
+            # "vim-typescript"    # TODO: https://github.com/kalbasit/dotfiles/issues/15
+            "yats-vim"
 
-            ]
-            ++ (if cfg.keyboardLayout == "colemak" then ["vim-colemak"] else []);
-        }
-      ];
-    };
+          ]
+          ++ (if keyboardLayout == "colemak" then ["vim-colemak"] else []);
+      }
+    ];
   };
 }
