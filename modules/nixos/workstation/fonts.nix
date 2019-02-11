bf3ffc1d2f7c47e1df2b5ac892678df796550fa7
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-02-10 21:14:52 -0800

    opt for the unstable b612 version

--- modules/nixos/workstation/fonts.nix
+++ modules/nixos/workstation/fonts.nix
@@ -2,33 +2,7 @@
 
 with lib;
 
-let
-
-  font-b612 = with pkgs; stdenv.mkDerivation {
-    name = "b612-font-2019-01-21";
-
-    src = fetchgit {
-      url = "git://git.polarsys.org/gitroot/b612/b612.git";
-      rev = "bd14fde2544566e620eab106eb8d6f2b7fb1347e";
-      sha256 = "1w1w9za599w3asmdkhng9amb9w0riq6mg400p43w1qnj0zqazy3d";
-    };
-
-    buildPhase = ":";
-
-    installPhase = ''
-      mkdir -p $out/share/fonts
-      cp ./TTF/*.ttf $out/share/fonts
-    '';
-
-    meta = {
-      homepage = "http://b612-font.com/";
-      platforms = stdenv.lib.platforms.all;
-      license = stdenv.lib.licenses.epl10;
-      maintainers = [ stdenv.lib.maintainers.grahamc ];
-    };
-  };
-
-in {
+{
   options.mine.workstation.fonts.enable = mkEnableOption "workstation.fonts";
 
   config = mkIf config.mine.workstation.fonts.enable {
@@ -50,8 +24,7 @@ in {
         # helvetica
         vegur # the official NixOS font
 
-        # TODO: This has already landed in unstable!
-        font-b612
+        unstable.b612
       ];
     };
   };
