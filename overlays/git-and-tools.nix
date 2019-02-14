30cdf0fcb5aa36589f0a7729ed2260b18592d1d7
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-02-14 08:59:40 -0800

    get rid of the git-and-tools overlay

--- overlays/git-and-tools.nix
+++
@@ -1,8 +0,0 @@
-self: super:
-
-{
-  # TODO: how to check for overlay assertions per host?
-  gitAndTools = /* assert ! super.gitAndTools ? git-appraise; */ super.gitAndTools // {
-    git-appraise = super.unstable.gitAndTools.git-appraise;
-  };
-}
