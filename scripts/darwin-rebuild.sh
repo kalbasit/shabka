622a081852717c7166d9435322ba4446b4506e49
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-02-14 22:10:04 -0800

    add darwin to nix_path [ci skip]

--- scripts/darwin-rebuild.sh
+++ scripts/darwin-rebuild.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 readonly shabka_path="$(cd $(dirname "${BASH_SOURCE[0]}")/../ && pwd)"
 readonly nixpkgs_stable="$( nix-build --no-out-link "${shabka_path}/external/nixpkgs-stable.nix" )"
 readonly nixpkgs_unstable="$( nix-build --no-out-link -E "with import (import ${shabka_path}/external/nixpkgs-stable.nix) {}; import ${shabka_path}/external/nixpkgs-unstable.nix { inherit runCommand fetchpatch; }" )"
+readonly nix_darwin="$( nix-build --no-out-link -E "with import ${nixpkgs} {}; import ${shabka_path}/external/nix-darwin.nix { inherit runCommand fetchpatch; }" )"
 
 # define all local variables
 host="$( hostname -s )"
@@ -60,4 +61,4 @@ fi
 unset NIX_PATH
 
 set -x
-darwin-rebuild -I nixpkgs="${nixpkgs}" -I "darwin-config=${darwin_config}" "${@}"
+darwin-rebuild -I nixpkgs="${nixpkgs}" -I "darwin=${nix_darwin}" -I "darwin-config=${darwin_config}" "${@}"
