4348067b715c7bd8cb4f4bdc3ff5d9c21fafa8c6
Wael Nasreddine <wael.nasreddine@gmail.com>
2019-02-15 04:19:58 +0100

    fix the grep

--- os-specific/darwin/setup.sh
+++ os-specific/darwin/setup.sh
@@ -69,7 +69,7 @@ if ! defaults read com.github.kalbasit.shabka bootstrap >/dev/null 2>&1; then
 
 		info "Installing Nix"
 		curl https://nixos.org/nix/install | sh
-		if ! grep -q nix.sh ~/.profile; then
+		if ! grep -q nix.sh ~/.profile 2>/dev/null; then
 			echo '. ~/.nix-profile/etc/profile.d/nix.sh' >> ~/.profile
 		fi
 		set +u
