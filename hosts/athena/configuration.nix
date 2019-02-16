2f88a9bee160ca0f29e798c077cd4c667d26ffd3
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-02-16 13:36:09 -0800

    disable the checks so I can switch without polluting my home directory

--- hosts/athena/configuration.nix
+++ hosts/athena/configuration.nix
@@ -10,4 +10,10 @@ with lib;
   ];
 
   networking.hostName = "athena";
+
+  # XXX: The checks fail when the nix-darwin does not find the configuration at
+  # ~/.nixpkgs/darwin-configuration.nix. We force it to empty string to disable
+  # these checks. Please see https://github.com/LnL7/nix-darwin/issues/117 for
+  # more information.
+  system.checks.text = mkForce "";
 }
