bc974bf454fb3fa781ce1e15fec871ac29ca144e
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-02-17 15:36:20 -0800

    the sleepimage must be in the initial setup only

--- modules/darwin/general/mac.nix
+++ modules/darwin/general/mac.nix
@@ -53,13 +53,6 @@
     # Disable hibernation (speeds up entering sleep mode)
     sudo pmset -a hibernatemode 0
 
-    # Remove the sleep image file to save disk space
-    sudo rm /private/var/vm/sleepimage
-    # Create a zero-byte file instead…
-    sudo touch /private/var/vm/sleepimage
-    # …and make sure it can’t be rewritten
-    sudo chflags uchg /private/var/vm/sleepimage<Plug>_
-
     ###############################################################################
     # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
     ###############################################################################
--- os-specific/darwin/setup.sh
+++ os-specific/darwin/setup.sh
@@ -116,4 +116,11 @@ fi
 	# the Dock to launch apps.
 	defaults write com.apple.dock persistent-apps -array
 
+	# Remove the sleep image file to save disk space
+	sudo rm /private/var/vm/sleepimage
+	# Create a zero-byte file instead…
+	sudo touch /private/var/vm/sleepimage
+	# …and make sure it can’t be rewritten
+	sudo chflags uchg /private/var/vm/sleepimage<Plug>_
+
 } # prevent the script from executing partially downloaded
