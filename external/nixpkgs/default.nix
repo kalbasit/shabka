262c43bc5558c5827f94d31f2ce87afbfe70b200
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-03-07 19:48:10 -0800

    pass the mkExternal down

--- external/nixpkgs/default.nix
+++ external/nixpkgs/default.nix
@@ -1,6 +1,6 @@
 { callPackage, mkExternal }:
 
 {
-  release-18-09 = callPackage ./18.09 { };
+  release-18-09 = callPackage ./18.09 { inherit mkExternal; };
   release-unstable = callPackage ./unstable { inherit mkExternal; };
 }
