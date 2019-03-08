9fcd884a9fbfe0896d2c473cfe104da4101b4da8
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-03-08 10:19:40 -0800

    actual alias it :manfacepalm:

--- modules/home/software/zsh/default.nix
+++ modules/home/software/zsh/default.nix
@@ -127,6 +127,7 @@ in {
         e = "\${EDITOR:-nvim}";
         gl = "github_commit_link";
         http = "http --print=HhBb";
+        kc = "kubectl";
         ll = "ls -la";
         pw = "ps aux | grep -v grep | grep -e";
         rot13 = "tr \"[A-Za-z]\" \"[N-ZA-Mn-za-m]\"";
