4b47660dbd49a7671ec4da1b73576502c05e14b1
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-03-22 20:30:07 -0700

    scripts/nixops: fix what 99d429 broke

--- scripts/nixops.sh
+++ scripts/nixops.sh
@@ -4,14 +4,7 @@ set -euo pipefail
 
 readonly shabka_path="$(cd $(dirname "${BASH_SOURCE[0]}")/../ && pwd)"
 
-# use nix-build to get the nixpkgs source path in the nix store. nix-shell can
-# be pointed to nixpkgs.nix as is and it's able to call the function to get the
-# actual source but for some reason this is not work with nixos-rebuild. See
-# https://gist.github.com/kalbasit/deec7b74b64f70d24ca1967883c8e7b6 for more
-# details.
-readonly nixpkgs_stable="${shabka_path}/external/nixpkgs-stable.nix"
-readonly nixpkgs="$( nix-build --no-out-link ${nixpkgs_stable} )"
-export NIX_PATH="nixpkgs=${nixpkgs}"
+source "${shabka_path}/lib/bash/rebuild-common.sh"
 
 readonly network_secrets="${HOME}/private/network-secrets"
 readonly state_location="shabka/deployments.nixops"
