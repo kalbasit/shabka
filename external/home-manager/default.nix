6a28043ed721d02c76fd97d95ca8f448c8bf2b09
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-04-12 22:25:04 -0700

    external/home-manager: update to a6f0fa90f73eab34744bd1dadbc73490e86be057
    Compare changes at https://github.com/rycee/home-manager/compare/f07510e2b63075a5e334e603f666c5c5838563ce...a6f0fa90f73eab34744bd1dadbc73490e86be057

--- external/home-manager/default.nix
+++ external/home-manager/default.nix
@@ -10,15 +10,6 @@ let
   patches = [
     # https://github.com/rycee/home-manager/pull/474
     ./474-fix-switch-user-without-profile.patch
-
-    # https://github.com/rycee/home-manager/pull/583
-    ./583-activate-hm-through-postactivation.patch
-
-    # https://github.com/rycee/home-manager/pull/586
-    ./586-install-packages-through-user-packages.patch
-
-    # https://github.com/rycee/home-manager/pull/587
-    ./587-login-as-user.patch
   ];
 
   patched = mkExternal {
