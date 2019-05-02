989e08e730a2b17a43e488b10c22227d924226ce
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-05-02 07:54:38 -0700

    alright, first import of what I already have

---
+++ modules/home/software/vscode/default.nix
@@ -0,0 +1,165 @@
+{ pkgs, lib, ... }:
+
+{
+  programs.vscode = {
+    enable = true;
+    userSettings = {
+      vim = {
+        enableNeovim = true;
+        hlsearch = true;
+        incsearch = true;
+        leader = ";";
+        neovimPath = "${lib.getBin pkgs.neovim}/bin/nvim";
+        useCtrlKeys = true;
+        useSystemClipboard = true;
+        changeWordIncludesWhitespace = true;
+        visualModeKeyBindingsNonRecursive = [
+          {
+            before = ["n"];
+            after = ["<left>"];
+          }
+          {
+            before = ["e"];
+            after = ["<down>"];
+          }
+          {
+            before = ["i"];
+            after = ["<up>"];
+          }
+          {
+            before = ["o"];
+            after = ["<right>"];
+          }
+          {
+            before = ["c"];
+            after = ["y"];
+          }
+          {
+            before = ["v"];
+            after = ["p"];
+          }
+        ];
+
+        normalModeKeyBindingsNonRecursive = [
+          {
+            before = ["n"];
+            after = ["<left>"];
+          }
+          {
+            before = ["e"];
+            after = ["<down>"];
+          }
+          {
+            before = ["i"];
+            after = ["<up>"];
+          }
+          {
+            before = ["o"];
+            after = ["<right>"];
+          }
+          {
+            before = ["s"];
+            after = ["i" ];
+          }
+          {
+            before = ["<leader>" "w" "w"];
+            after = [];
+            commands = [
+              {
+                command = ":wall";
+              }
+            ];
+          }
+          {
+            before = ["h"];
+            after = ["o"];
+          }
+          {
+            before = ["H"];
+            after = ["O"];
+          }
+          {
+            before = ["<c-j>"];
+            after = ["<c-n>"];
+          }
+          {
+            before = ["k"];
+            after = ["n"];
+          }
+          {
+            before = ["K"];
+            after = ["N"];
+          }
+          {
+            before = ["u"];
+            after = ["i"];
+          }
+          {
+            before = ["U"];
+            after = ["I"];
+          }
+          {
+            before = ["l"];
+            after = ["u"];
+          }
+          {
+            before = ["L"];
+            after = ["U"];
+          }
+          {
+            before = ["N"];
+            after = ["J"];
+          }
+          {
+            before = ["E"];
+            after = ["K"];
+          }
+          {
+            before = ["I"];
+            after = ["L"];
+          }
+          {
+            before = ["j"];
+            after = ["e"];
+          }
+          {
+            before = ["J"];
+            after = ["E"];
+          }
+          {
+            before = ["t"];
+            after = ["a"];
+          }
+          {
+            before = ["T"];
+            after = ["A"];
+          }
+          {
+            before = ["c"];
+            after = ["y"];
+          }
+          {
+            before = ["C"];
+            after = ["Y"];
+          }
+          {
+            before = ["v"];
+            after = ["p"];
+          }
+          {
+            before = ["V"];
+            after = ["P"];
+          }
+          {
+            before = ["a"];
+            after = ["v"];
+          }
+          {
+            before = ["A"];
+            after = ["V"];
+          }
+        ];
+      };
+    };
+  };
+}
