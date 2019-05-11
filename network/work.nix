0f3f6e0d7c0f93114b67a9bc27f38f089beee6f4
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-05-10 22:53:28 -0700

    create the full VPC

--- network/work.nix
+++ network/work.nix
@@ -1,13 +1,84 @@
 let
+  lib = (import <nixpkgs> {}).lib;
   secrets = import /yl/private/network-secrets/shabka/network/work.nix;
 in {
   network.description = "Network at work";
   network.enableRollback = true;
 
   resources = {
+    vpc.nixops = {
+      inherit (secrets) accessKeyId cidrBlock region;
+
+      enableDnsSupport = true;
+
+      tags = {
+        Source = "NixOps";
+      };
+    };
+
+    vpcSubnets =
+      let
+        subnet = {cidr, zone}:
+          { resources, ... }:
+          {
+            inherit (secrets) accessKeyId region zone;
+            vpcId = resources.vpc.nixops;
+            cidrBlock = cidr;
+            mapPublicIpOnLaunch = true;
+            tags = {
+              Source = "NixOps";
+            };
+          };
+      in
+      {
+        subnet-a = subnet { inherit (secrets) zone; cidr = secrets.cidrBlock; };
+      };
+
+    vpcRouteTables =
+      {
+        route-table =
+          { resources, ... }:
+          {
+            inherit (secrets) region accessKeyId;
+            vpcId = resources.vpc.nixops;
+          };
+      };
+
+    vpcRouteTableAssociations =
+      let
+        subnets = ["subnet-a"];
+        association = subnet:
+          { resources, ... }:
+          {
+            inherit (secrets) region accessKeyId;
+            subnetId = resources.vpcSubnets."${subnet}";
+            routeTableId = resources.vpcRouteTables.route-table;
+          };
+      in
+        (builtins.listToAttrs (map (s: lib.nameValuePair "association-${s}" (association s) ) subnets));
+
+    vpcRoutes = {
+      igw-route =
+        { resources, ... }:
+        {
+          inherit (secrets) region accessKeyId;
+          routeTableId = resources.vpcRouteTables.route-table;
+          destinationCidrBlock = "0.0.0.0/0";
+          gatewayId = resources.vpcInternetGateways.igw;
+        };
+    };
+
+    vpcInternetGateways.igw =
+      { resources, ... }:
+      {
+        inherit (secrets) region accessKeyId;
+        vpcId = resources.vpc.nixops;
+      };
+
     ec2SecurityGroups = {
-      "ssh-in" = {
+      ssh-in = { resources, ... }: {
         inherit (secrets) accessKeyId region;
+        vpcId = resources.vpc.nixops;
         description = "Allow incoming SSH connection from anywhere";
         rules = [
           {fromPort = 22; toPort = 22; protocol = "tcp"; sourceIp = "0.0.0.0/0"; }
@@ -23,13 +94,20 @@ in {
     deployment = {
       targetEnv = "ec2";
       ec2 = {
-        inherit (secrets) accessKeyId region keyPair ami;
+        inherit (secrets) accessKeyId ami keyPair region;
+
+        subnetId = resources.vpcSubnets.subnet-a;
 
         instanceType = "t3.2xlarge";
+        ebsInitialRootDiskSize = 1024;
+        associatePublicIpAddress = true;
 
-        securityGroups = [
-          resources.ec2SecurityGroups.ssh-in
-        ];
+        securityGroupIds = [ resources.ec2SecurityGroups.ssh-in.name ];
+
+        tags = {
+          Source = "NixOps";
+          Owner = "wael";
+        };
       };
     };
   };
