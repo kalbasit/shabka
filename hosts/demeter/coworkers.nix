d7deb001b5ee62dee678e654708b228ac17608ac
Wael Nasreddine <wael.nasreddine@gmail.com>
2019-05-11 12:38:11 -0700

    hosts/demeter: create a user for each of my coworkers (#209)

---
+++ hosts/demeter/coworkers.nix
@@ -0,0 +1,39 @@
+{ lib, ... }:
+
+with lib;
+
+let
+  # expected format:
+  #   {
+  #     # name
+  #     github-username = {
+  #       uid = nnnn;
+  #       sshAuthorizedKeys = with builtins; readFile (fetchurl {
+  #         url = https://github.com/github-username.keys;
+  #         sha256 = "0000000000000000000000000000000000000000000000000000";
+  #       });
+  #     };
+  #   }
+  coworkersExpr = /yl/private/network-secrets/shabka/hosts/demeter/coworkers;
+
+  coworkers = optionalAttrs (builtins.pathExists coworkersExpr) (import coworkersExpr);
+
+  makeUser = userName: { uid, sshAuthorizedKeys, home ? "/home/${userName}" }: nameValuePair
+    userName
+    {
+      inherit home uid;
+
+      group = "users";
+      extraGroups = [
+        "builders"
+        "fuse"
+      ];
+
+      isNormalUser = true;
+
+      openssh.authorizedKeys.keys = singleton sshAuthorizedKeys;
+    };
+
+in {
+  users.users = (mapAttrs' makeUser coworkers);
+}
