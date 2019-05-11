ce73f477668390318af113ee41c07a53aabf853a
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-05-11 09:52:09 -0700

    disable nasreddineCA on my server, will never put VPN on it

--- hosts/demeter/configuration.nix
+++ hosts/demeter/configuration.nix
@@ -2,15 +2,7 @@
 
 with lib;
 
-let
-  shabka = import <shabka> { };
-
-  nasreddineCA = builtins.readFile (builtins.fetchurl {
-    url = "https://s3-us-west-1.amazonaws.com/nasreddine-infra/ca.crt";
-    sha256 = "17x45njva3a535czgdp5z43gmgwl0lk68p4mgip8jclpiycb6qbl";
-  });
-
-in {
+{
   imports = [
     ../../modules/nixos
 
@@ -45,10 +37,6 @@ in {
 
   mine.useColemakKeyboardLayout = true;
   mine.virtualisation.docker.enable = true;
-
-  security.pki.certificates = [
-    nasreddineCA
-  ];
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
