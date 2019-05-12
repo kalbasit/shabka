fe7ed501d30ece834dcb0b793f6403b5874b2d2f
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-05-11 23:11:23 -0700

    commiting whatever, not sure I did these changes long ago

--- external/nixpkgs/default.nix
+++ external/nixpkgs/default.nix
@@ -2,5 +2,6 @@
 
 {
   release-18-09 = import ./18.09 { inherit mkExternal; };
+  release-19-03 = import ./19.03 { inherit mkExternal; };
   release-unstable = import ./unstable { inherit mkExternal; };
 }
