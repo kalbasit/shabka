c3893fea27c014ff8292d8e6ad612c017e327933
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-05-15 23:15:13 -0700

    add a script to push to cachix

---
+++ scripts/push-to-cachix.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+readonly shabka_path="$(cd $(dirname "${BASH_SOURCE[0]}")/../ && pwd)"
+
+# I don't want to ever make the mistake of pushing while my private files are
+# accessible. This is meant to go away eventually, see #216 for more
+# information
+if [[ -d /yl/private/network-secrets ]]; then
+    >&2 echo "ERR: /yl/private/network-secrets exists and is accessible. Will not continue!"
+    exit 1
+fi
+
+for host in "${shabka_path}"/hosts/*; do
+    if grep -q '\<nixos\>' "${host}/default.nix"; then
+        echo "Pushing the cache for ${host}"
+        nix-build --option builders '' "${host}" -A nixos | cachix push yl
+    fi
+done
