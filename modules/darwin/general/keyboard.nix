12f8b26ed77c94d8c3c186b830179048b8795437
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-06-14 23:51:49 -0700

    fix formatting

--- modules/darwin/general/keyboard.nix
+++ modules/darwin/general/keyboard.nix
@@ -45,8 +45,8 @@ with lib;
     # TODO: This is just colemak, add the equivalent for rest.
     system.activationScripts.postActivation = mkIf (builtins.toString (head cfg.layouts) == "colemak") {
       text = ''
-      # set the keyboard layout to Colemak
-      # https://github.com/hjuutilainen/dotfiles/blob/e8861a756df35cf7ade9fd964eb4f6c07ed5264b/bin/macos-system-defaults.sh#L100
+        # set the keyboard layout to Colemak
+        # https://github.com/hjuutilainen/dotfiles/blob/e8861a756df35cf7ade9fd964eb4f6c07ed5264b/bin/macos-system-defaults.sh#L100
         sudo defaults write /Library/Preferences/com.apple.HIToolbox AppleCurrentKeyboardLayoutInputSourceID com.apple.keylayout.Colemak
         sudo defaults write /Library/Preferences/com.apple.HIToolbox AppleDefaultAsciiInputSource -dict InputSourceKind "Keyboard Layout" "KeyboardLayout ID" -int 12825 "KeyboardLayout Name" Colemak
         sudo defaults delete /Library/Preferences/com.apple.HIToolbox AppleEnabledInputSources
