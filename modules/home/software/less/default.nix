a9f878a1b415937b21f3859eac073cd7725c78d4
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-06-15 09:29:18 -0700

    merge lesskey into the default as a string

--- modules/home/software/less/default.nix
+++ modules/home/software/less/default.nix
@@ -3,28 +3,26 @@
 with lib;
 
 let
-  less-config = pkgs.stdenvNoCC.mkDerivation rec {
-    name = "less-config";
+  colemakKeybindings = ''
+    n left-scroll
+    e forw-line
+    i back-line
+    o right-scroll
+    k repeat-search
+    K reverse-search
+  '';
 
-    phases = [ "buildPhase" "installPhase" ];
-
-    src = ./.;
-
-    buildInputs = [ pkgs.less ];
-
-    buildPhase = ''
-      lesskey -o less $src/lesskey
-    '';
-
-    installPhase = ''
-      install -Dm644 less $out/share/less/less
-    '';
-  };
+  lessKey = ''
+    # NOTE: use --quit-if-one-screen only if --no-init is set. Otherwise less will
+    # print nothing and exits which looks like it's crashing.
+    LESS= --RAW-CONTROL-CHARS --no-init --quit-if-one-screen --ignore-case
+  ''
+  + optionalString ((builtins.head config.mine.keyboard.layouts) == "colemak") colemakKeybindings;
 
 in {
   options.mine.less.enable = mkEnableOption "less";
 
-  config = mkIf (config.mine.less.enable && (builtins.head config.mine.keyboard.layouts) == "colemak" ) {
-    home.file.".less".source = "${less-config}/share/less/less";
+  config = mkIf (config.mine.less.enable) {
+    home.file.".less".text = lessKey;
   };
 }
