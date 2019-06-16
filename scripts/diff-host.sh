6905b9d832ac6f812559f63037d187f154439f59
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-06-15 17:32:37 -0700

    scripts/diff-host.sh: diff a host against two revisions

---
+++ scripts/diff-host.sh
@@ -0,0 +1,52 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p nix-diff
+
+set -euo pipefail
+
+readonly shabka_path="$(cd $(dirname "${BASH_SOURCE[0]}")/../ && pwd)"
+
+usage() {
+    >&2 echo "USAGE: $0 <host> <base-rev> [target-rev]"
+    >&2 echo "If no target-rev is given, HEAD will be used"
+}
+
+if [[ "${#}" -gt 0 ]] && [[ "${1}" = "-h" ]] && [[ "${1}" = "--help" ]]; then
+    usage
+    exit 0
+fi
+
+if [[ "${#}" -lt 2 ]]; then
+    usage
+    exit 1
+fi
+
+readonly host="${1}"
+readonly base_rev="${2}"
+
+if [[ "${#}" -ge 3 ]]; then
+    readonly target_rev="${3}"
+else
+    readonly target_rev="$(git rev-parse HEAD)"
+fi
+
+readonly wd="$(mktemp -d)"
+trap "rm -rf ${wd}" EXIT
+
+cd "${wd}"
+
+(
+    git clone "${shabka_path}" base
+    cd base
+    git checkout "${base_rev}"
+)
+
+(
+    git clone "${shabka_path}" target
+    cd target
+    git checkout "${target_rev}"
+)
+
+"${shabka_path}/scripts/build-host.sh" --shabka-path "${wd}/base" "${host}" --out-link base-result
+"${shabka_path}/scripts/build-host.sh" --shabka-path "${wd}/target" "${host}" --out-link target-result
+
+nix-diff "$(nix-store -q --deriver base-result)" "$(nix-store -q --deriver target-result)"
