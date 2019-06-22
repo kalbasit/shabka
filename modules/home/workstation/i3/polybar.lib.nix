1fb157eeed8730626861ede99ed341b37cc1c794
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-06-22 11:14:59 -0700

    get rid of the quit logic, systemd already handles that with the forking

--- modules/home/workstation/i3/polybar.lib.nix
+++ modules/home/workstation/i3/polybar.lib.nix
@@ -2,21 +2,10 @@
 
 let
   script = ''
-    # Terminate already running bar instances
-    kill $(${pkgs.procps}/bin/pgrep -f polybar | ${pkgs.gnugrep}/bin/grep -v ^$$\$)
-
-    # Wait until the processes have been shut down
-    while ${pkgs.procps}/bin/pgrep -f polybar | ${pkgs.gnugrep}/bin/grep -v ^$$\$ >/dev/null; do ${pkgs.coreutils}/bin/sleep 1; done
-
-    if type "xrandr"; then
-      for m in $(${pkgs.xorg.xrandr}/bin/xrandr --query | ${pkgs.gnugrep}/bin/grep " connected" | ${pkgs.coreutils}/bin/cut -d" " -f1); do
-        MONITOR=$m polybar --reload top &
-      done
-    else
-      polybar --reload top &
-    fi
-
-    echo "Bars launched..."
+    for m in $(${pkgs.xorg.xrandr}/bin/xrandr --query | ${pkgs.gnugrep}/bin/grep " connected" | ${pkgs.coreutils}/bin/cut -d" " -f1); do
+      echo "Starting polybar on monitor $m"
+      MONITOR=$m polybar --reload top &
+    done
   '';
 in {
   enable = config.mine.workstation.i3.bar.engine == "polybar";
