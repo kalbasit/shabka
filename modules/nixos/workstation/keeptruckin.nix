f2009cc1ac83f41196a2040f5da9703142b4d221
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-07-01 12:54:22 -0700

    [nixos/workstation] Fix keeptruckin hostname

--- modules/nixos/workstation/keeptruckin.nix
+++ modules/nixos/workstation/keeptruckin.nix
@@ -13,7 +13,7 @@ in {
   config = mkIf cfg.enable {
     # Add the extra hosts
     networking.extraHosts = ''
-      127.0.0.1 docker.keeptruckin.dev
+      127.0.0.1 docker.keeptruckin.dev docker.keeptruckin.work
     '';
   };
 }
