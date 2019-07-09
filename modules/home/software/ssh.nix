bd6d4c5b36067458be961ae40552188a53cca1be
risson <18313093+rissson@users.noreply.github.com>
2019-07-09 23:21:40 +0200

    home: ssh: hardening default configuration (#248)

--- modules/home/software/ssh.nix
+++ modules/home/software/ssh.nix
@@ -11,7 +11,8 @@ in {
     enable = mkEnableOption "SSH configurations";
 
     privateSSHPath = mkOption {
-      type = types.path;
+      type = types.nullOr types.path;
+      default = null;
       defaultText = ''
         The path to the private SSH module
       '';
@@ -19,13 +20,17 @@ in {
   };
 
   config = mkIf cfg.enable {
-    assertions = [
-      {
-        assertion = builtins.pathExists cfg.privateSSHPath;
-        message = "privateSSHPath must exist";
-      }
-    ];
 
-    programs.ssh = if cfg.enable then import cfg.privateSSHPath else {};
+    programs.ssh = if (cfg.privateSSHPath != null) then import cfg.privateSSHPath else {
+        enable = true;
+
+        extraConfig = ''
+          KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
+          PubkeyAuthentication yes
+          HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa
+          Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+          MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
+        '';
+    };
   };
 }
