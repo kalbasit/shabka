c083bc7af16a5acc1d4b70a41010e1ac37264514
risson <marcschmitt2@gmail.com>
2019-07-09 17:18:52 +0200

    Start

--- modules/home/software/ssh.nix
+++ modules/home/software/ssh.nix
@@ -19,13 +19,17 @@ in {
   };
 
   config = mkIf cfg.enable {
-    assertions = [
-      {
-        assertion = builtins.pathExists cfg.privateSSHPath;
-        message = "privateSSHPath must exist";
-      }
-    ];
 
-    programs.ssh = if cfg.enable then import cfg.privateSSHPath else {};
+    programs.ssh = if (builtins.pathExists cfg.privateSSHPath) then import cfg.privateSSHPath else {
+        enable = true;
+
+        extraConfig = ''
+          KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
+          PubkeyAuthentication yes
+          HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa
+          Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+          MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
+        '';
+    };
   };
 }
