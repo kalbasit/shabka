1a432d8c4b630ef94d5d009254bb1d88121be52f
risson <marcschmitt2@gmail.com>
2019-07-12 13:25:34 +0200

    Testing

--- modules/home/workstation/urxvt.nix
+++ modules/home/workstation/urxvt.nix
@@ -6,30 +6,34 @@ let
   cfg = config.shabka.workstation.urxvt;
 in
 {
-  options.shabka.workstation.urxvt.enable = mkEnableOption "workstation.urxvt";
+  options.shabka.workstation.urxvt = {
+    enable = mkEnableOption "workstation.urxvt";
+
+    transparency = mkEnableOption "Enable transparency on workstation.urxvt.";
+    scrollOnOutput = mkEnableOption "workstation.urxvt Scroll on output";
+  };
 
   config = mkIf cfg.enable {
+
+    home.packages = with pkgs; [ urxvt_font_size ];
 
     programs.urxvt = {
 
       enable = true;
-      package = pkgs.rxvt_unicode;
+      package = pkgs.rxvt_unicode-with-plugins;
 
       fonts = [ "xft:SourceCodePro:style:Regular:size=9:antialias=true" ];
 
       keybindings = {
-        "M-u" = "perl:url-select:select_next";
-        "M-Escape" = "perl:keyboard-select:activate";
-        "M-s" = "perl:keyboard-select:search";
         "Shift-Control-V" = "eval:paste_clipboard";
         "Shift-Control-C" = "eval:selection_to_clipboard";
         "Control-Up" = "\\033[1;5A";
         "Control-Down" = "\\033[1;5B";
         "Control-Left" = "\\033[1;5D";
         "Control-Right" = "\\033[1;5C";
-        "Control-minus" = "resize-font:smaller";
-        "Control-plus" = "resize-font:bigger";
-        "Control-equal" = "resize-font:reset";
+        #"Control-minus" = "resize-font:smaller";
+        #"Control-plus" = "resize-font:bigger";
+        #"Control-equal" = "resize-font:reset";
       };
 
       scroll = {
@@ -37,17 +41,15 @@ in
         keepPosition = true;
         lines = 10000;
         scrollOnKeystroke = true;
-        scrollOnOutput = false;
+        scrollOnOutput = cfg.scrollOnOutput;
       };
 
-      shading = 30;
-      transparent = true;
+      shading = if cfg.transparency then 30 else 100;
+      transparent = cfg.transparency;
 
       extraConfig = {
-        perl-ext-common = "default,tabbed,matcher,resize-font,-tabbed";
+        perl-ext-common = "default,resize-font";
       };
-
     };
-
   };
 }
