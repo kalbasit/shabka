94a84045e7f98602fddb3fe3d892542aa2bbec74
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-08-07 16:45:41 -0700

    home: workstation: autorandr: restart polybar post-switch

--- modules/home/workstation/autorandr.nix
+++ modules/home/workstation/autorandr.nix
@@ -14,34 +14,44 @@ in {
       enable = true;
 
       hooks = {
-         postswitch = {
-          "move-workspaces-to-main" = ''
-            set -euo pipefail
+        postswitch = mkMerge [
+          {
+            "move-workspaces-to-main" = ''
+              set -euo pipefail
 
-            # Make sure that i3 is running
-            if [[ "$( ${getBin pkgs.i3}/bin/i3-msg -t get_outputs | ${getBin pkgs.jq}/bin/jq -r '.[] | select(.active == true) | .name' | wc -l )" -eq 1 ]]; then
-              echo "no other monitor, bailing out"
-              exit 0
-            fi
+              # Make sure that i3 is running
+              if [[ "$( ${getBin pkgs.i3}/bin/i3-msg -t get_outputs | ${getBin pkgs.jq}/bin/jq -r '.[] | select(.active == true) | .name' | wc -l )" -eq 1 ]]; then
+                echo "no other monitor, bailing out"
+                exit 0
+              fi
 
-            # Figure out the identifier of the main monitor
-            readonly main_monitor="$( ${getBin pkgs.i3}/bin/i3-msg -t get_outputs | ${getBin pkgs.jq}/bin/jq -r '.[] | select(.active == true and .primary == true) | .name' )"
+              # Figure out the identifier of the main monitor
+              readonly main_monitor="$( ${getBin pkgs.i3}/bin/i3-msg -t get_outputs | ${getBin pkgs.jq}/bin/jq -r '.[] | select(.active == true and .primary == true) | .name' )"
 
-            # Get the list of workspaces that are not on the main monitor
-            readonly workspaces=( $(${getBin pkgs.i3}/bin/i3-msg -t get_workspaces | ${getBin pkgs.jq}/bin/jq -r '.[] | select(.output != "''${main_monitor}") | .name') )
+              # Get the list of workspaces that are not on the main monitor
+              readonly workspaces=( $(${getBin pkgs.i3}/bin/i3-msg -t get_workspaces | ${getBin pkgs.jq}/bin/jq -r '.[] | select(.output != "''${main_monitor}") | .name') )
 
-            # Move all workspaces over
-            for workspace in "''${workspaces[@]}"; do
-              ${getBin pkgs.i3}/bin/i3-msg "workspace ''${workspace}; move workspace to output ''${main_monitor}"
-            done
+              # Move all workspaces over
+              for workspace in "''${workspaces[@]}"; do
+                ${getBin pkgs.i3}/bin/i3-msg "workspace ''${workspace}; move workspace to output ''${main_monitor}"
+              done
 
-            # Move the Slack workspace to the internal screen
-            ${getBin pkgs.i3}/bin/i3-msg "workspace slack; move workspace to output eDP-1"
+              # Move the Slack workspace to the internal screen
+              ${getBin pkgs.i3}/bin/i3-msg "workspace slack; move workspace to output eDP-1"
 
-            # Go to my personal workspace
-            ${getBin pkgs.i3}/bin/i3-msg "workspace personal@base"
-          '';
-        };
+              # Go to my personal workspace
+              ${getBin pkgs.i3}/bin/i3-msg "workspace personal@base"
+            '';
+          }
+
+          (mkIf config.shabka.workstation.i3.bar.polybar.enable {
+            "restart-polybar" = ''
+              set -euo pipefail
+
+              systemctl --user restart polybar.service
+            '';
+          })
+        ];
       };
     };
   };
