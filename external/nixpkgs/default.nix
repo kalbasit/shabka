e142d0fd3fd3c725db993f25acf4e3089799f2cb
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-10-08 17:11:46 -0700

    external: create the 19.09 nixpkgs release

--- external/nixpkgs/default.nix
+++ external/nixpkgs/default.nix
@@ -2,5 +2,6 @@
 
 {
   release-19-03 = import ./19.03 { inherit mkExternal; };
+  release-19-09 = import ./19.09 { inherit mkExternal; };
   release-unstable = import ./unstable { inherit mkExternal; };
 }
