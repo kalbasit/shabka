048d2b79e0a54ad0b5ee4ec10fe3b4f84f3c2553
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2019-10-08 19:01:39 -0700

    remove 19.03

--- external/nixpkgs/default.nix
+++ external/nixpkgs/default.nix
@@ -1,7 +1,6 @@
 { mkExternal }:
 
 {
-  release-19-03 = import ./19.03 { inherit mkExternal; };
   release-19-09 = import ./19.09 { inherit mkExternal; };
   release-unstable = import ./unstable { inherit mkExternal; };
 }
