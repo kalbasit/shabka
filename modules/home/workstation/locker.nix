406c41c08bc7251ce786d6efad65169b4ee79a4e
risson <marcschmitt2@gmail.com>
2019-10-11 13:45:28 +0200

    home: workstation: locker: caffeine-ng as a service

--- modules/home/workstation/locker.nix
+++ modules/home/workstation/locker.nix
@@ -11,6 +11,22 @@ with lib;
       lockCmd = "${pkgs.i3lock-color}/bin/i3lock-color --clock --color=606060";
       inactiveInterval = 15;
     };
-    home.packages = with pkgs; [ caffeine-ng ];
+
+    systemd.user.services.caffeine-ng = {
+      Unit = {
+        Description = "Caffeine-ng, a locker inhibitor";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
+
+      Service = {
+        #Environment="PATH=${pkgs.xautolock}/bin:$PATH";
+        ExecStart = "PATH=${pkgs.xautolock}/bin:$PATH ${pkgs.caffeine-ng}/bin/caffeine";
+      };
+    };
   };
 }
