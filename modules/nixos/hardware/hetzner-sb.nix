ce998e313e2207138f365292ddfe4d3e3a8569a2
risson <18313093+rissson@users.noreply.github.com>
2019-12-29 20:08:00 +0100

    nixos: hardware: add Hetzner dedicated server SB (#283)

---
+++ modules/nixos/hardware/hetzner-sb.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+{
+  config = mkIf (config.shabka.hardware.machine == "hetzner-sb") {
+    boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" "r8169" ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [ ];
+
+    nix.maxJobs = mkDefault 8;
+
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+
+    i18n.consoleFont = "Lat2-Terminus16";
+  };
+}
