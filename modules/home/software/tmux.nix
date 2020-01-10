473de1b260af46ae0d1765188a6a32e8aed1438e
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-01-10 05:50:34 +0100

    home: tmux: move theme settings to home/theme

--- modules/home/software/tmux.nix
+++ modules/home/software/tmux.nix
@@ -6,117 +6,6 @@ let
 
   cfg = config.shabka.tmux;
 
-  # TODO(high): Each color theme is defining it's own status format. The format
-  # should be unified and nix should interpolate to set the correct format
-
-  defaultTheme = gruvBox256Color;
-
-  # TODO(high): Move this over to the theme once TMux module is merged upstream
-  # in home-manager and this nixos module has been moved to the home module.
-  # See https://github.com/rycee/home-manager/pull/388.
-  seoul256Color = ''
-    set-option -g status-justify left
-    set-option -g status-left-length 16
-    set-option -g status-interval 60
-
-    set-option -g status-left '#[bg=colour72] #[bg=colour237] #[bg=colour236] #{prefix_highlight} #[bg=colour235]#[fg=colour185] #h #[bg=colour236] '
-    set-option -g status-right '#[bg=colour236] #[bg=colour237]#[fg=colour185] #[bg=colour235] #(date "+%a %b %d %H:%M") #[bg=colour236] #[bg=colour237] #[bg=colour72] '
-
-    set-window-option -g window-status-format '#[bg=colour238]#[fg=colour107] #I #[bg=colour239]#[fg=colour110] #[bg=colour240]#W#[bg=colour239]#[fg=colour195]#F#[bg=colour238] '
-    set-window-option -g window-status-current-format '#[bg=colour236]#[fg=colour215] #I #[bg=colour235]#[fg=colour167] #[bg=colour234]#W#[bg=colour235]#[fg=colour195]#F#[bg=colour236] '
-  ''
-  + (if versionAtLeast (builtins.parseDrvName pkgs.tmux.name).version "2.9" then ''
-      set-option -g status-style bg=colour237
-      set-option -g pane-active-border-style fg=colour215
-      set-option -g pane-border-style fg=colour185
-    '' else ''
-      set-option -g status-bg colour237
-      set-option -g pane-active-border-fg colour215
-      set-option -g pane-border-fg colour185
-
-    '');
-
-  gruvBox256Color = ''
-    # pane number display
-    set-option -g display-panes-active-colour colour250 #fg2
-    set-option -g display-panes-colour colour237 #bg1
-
-    # clock
-    set-window-option -g clock-mode-colour colour109 #blue
-
-    # bell
-    set-window-option -g window-status-bell-style fg=colour235,bg=colour167 #bg, red
-
-    ## Theme settings mixed with colors (unfortunately, but there is no cleaner way)
-    set-option -g status-justify "left"
-    set-option -g status-left-length "80"
-    set-option -g status-right-length "80"
-    set-window-option -g window-status-separator ""
-
-    set-option -g status-left "#[fg=colour248, bg=colour241] #S #[fg=colour241, bg=colour237, nobold, noitalics, nounderscore]"
-    set-option -g status-right "#[fg=colour239, bg=colour237, nobold, nounderscore, noitalics]#{prefix_highlight}#[fg=colour246,bg=colour239] %Y-%m-%d  %H:%M #[fg=colour248, bg=colour239, nobold, noitalics, nounderscore]#[fg=colour237, bg=colour248] #h "
-
-    set-window-option -g window-status-current-format "#[fg=colour239, bg=colour248, :nobold, noitalics, nounderscore]#[fg=colour239, bg=colour214] #I #[fg=colour239, bg=colour214, bold] #W #[fg=colour214, bg=colour237, nobold, noitalics, nounderscore]"
-    set-window-option -g window-status-format "#[fg=colour237,bg=colour239,noitalics]#[fg=colour223,bg=colour239] #I #[fg=colour223, bg=colour239] #W #[fg=colour239, bg=colour237, noitalics]"
-  ''
-  + (if versionAtLeast (builtins.parseDrvName pkgs.tmux.name).version "2.9" then ''
-      # default statusbar colors
-      set-option -g status-style bg=colour237,fg=colour223,none
-      set-option -g status-left-style none
-      set-option -g status-right-style none
-
-      # default window title colors
-      set-window-option -g window-status-style bg=colour214,fg=colour237,none
-
-      set-window-option -g window-status-activity-style bg=colour237,fg=colour248,none
-
-      # active window title colors
-      set-window-option -g window-status-current-style bg=default,fg=colour237
-
-      # pane border
-      set-option -g pane-active-border-style fg=colour250,fg=colour237
-
-      # message infos
-      set-option -g message-style bg=colour239,fg=colour223
-
-      # writting commands inactive
-      set-option -g message-command-style bg=colour239,fg=colour223
-    '' else ''
-      # default statusbar colors
-      set-option -g status-bg colour237 #bg1
-      set-option -g status-fg colour223 #fg1
-
-      # default window title colors
-      set-window-option -g window-status-bg colour214 #yellow
-      set-window-option -g window-status-fg colour237 #bg1
-
-      set-window-option -g window-status-activity-bg colour237 #bg1
-      set-window-option -g window-status-activity-fg colour248 #fg3
-
-      # active window title colors
-      set-window-option -g window-status-current-bg default
-      set-window-option -g window-status-current-fg colour237 #bg1
-
-      # pane border
-      set-option -g pane-active-border-fg colour250 #fg2
-      set-option -g pane-border-fg colour237 #bg1
-
-      # message infos
-      set-option -g message-bg colour239 #bg2
-      set-option -g message-fg colour223 #fg1
-
-      # writting commands inactive
-      set-option -g message-command-bg colour239 #fg3
-      set-option -g message-command-fg colour223 #bg1
-
-      set-option -g status-attr "none"
-      set-option -g status-left-attr "none"
-      set-option -g status-right-attr "none"
-      set-window-option -g window-status-activity-attr "none"
-      set-window-option -g window-status-attr "none"
-    '');
-
-
   tmuxVimAwarness = ''
     # Smart pane switching with awareness of Vim splits.
     # See: https://github.com/christoomey/vim-tmux-navigator
@@ -218,7 +107,6 @@ in {
       shortcut = "t";
 
       extraConfig = ''
-        ${defaultTheme}
         ${tmuxVimAwarness}
 
         #
--- modules/home/theme/seoul256-dark/default.nix
+++ modules/home/theme/seoul256-dark/default.nix
@@ -142,5 +142,26 @@ in {
     };
 
     programs.taskwarrior.colorTheme = "solarized-dark-256";
+
+    programs.tmux.extraConfig = ''
+      set-option -g status-justify left
+      set-option -g status-left-length 16
+      set-option -g status-interval 60
+
+      set-option -g status-left '#[bg=colour72] #[bg=colour237] #[bg=colour236] #{prefix_highlight} #[bg=colour235]#[fg=colour185] #h #[bg=colour236] '
+      set-option -g status-right '#[bg=colour236] #[bg=colour237]#[fg=colour185] #[bg=colour235] #(date "+%a %b %d %H:%M") #[bg=colour236] #[bg=colour237] #[bg=colour72] '
+
+      set-window-option -g window-status-format '#[bg=colour238]#[fg=colour107] #I #[bg=colour239]#[fg=colour110] #[bg=colour240]#W#[bg=colour239]#[fg=colour195]#F#[bg=colour238] '
+      set-window-option -g window-status-current-format '#[bg=colour236]#[fg=colour215] #I #[bg=colour235]#[fg=colour167] #[bg=colour234]#W#[bg=colour235]#[fg=colour195]#F#[bg=colour236] '
+    ''
+    + (if versionAtLeast (builtins.parseDrvName pkgs.tmux.name).version "2.9" then ''
+      set-option -g status-style bg=colour237
+      set-option -g pane-active-border-style fg=colour215
+      set-option -g pane-border-style fg=colour185
+    '' else ''
+      set-option -g status-bg colour237
+      set-option -g pane-active-border-fg colour215
+      set-option -g pane-border-fg colour185
+    '');
   };
 }
