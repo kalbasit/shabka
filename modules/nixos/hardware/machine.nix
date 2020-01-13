500c7663151d7724149ba6206f95e8f7ab538e65
Wael Nasreddine <wael.nasreddine@gmail.com>
2020-01-13 14:27:52 -0800

    allow shabka.hardware.machine to be null (#302)

--- modules/nixos/hardware/machine.nix
+++ modules/nixos/hardware/machine.nix
@@ -5,14 +5,18 @@ with lib;
 {
   options.shabka.hardware.machine = mkOption {
     description = "The machine name (usually model).";
-    type = types.enum [
-      "cloud"
-      "hetzner_cloud"
-      "hetzner-sb"
-      "precision-7530"
-      "thinkpad-e580"
-      "xps-13"
-      "zeus"
-    ];
+    default = null;
+    type = with types;
+      let
+        supportedMachines = enum [
+          "cloud"
+          "hetzner_cloud"
+          "hetzner-sb"
+          "precision-7530"
+          "thinkpad-e580"
+          "xps-13"
+          "zeus"
+        ];
+      in nullOr supportedMachines;
   };
 }
