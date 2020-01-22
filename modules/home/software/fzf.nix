fbcd0b9b5a3fdcc9e7af5c7795492abd9ed4fdc1
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-01-23 00:27:10 +0100

    apply requested changes

--- modules/home/software/fzf.nix
+++ modules/home/software/fzf.nix
@@ -7,8 +7,8 @@ let
 in {
   options.shabka.fzf.enable = mkEnableOption "Enable fzf - a command-line fuzzy finder.";
 
-  config.programs.fzf = {
-    enable = cfg.enable;
+  config.programs.fzf = mkIf cfg.enable {
+    enable = true;
     defaultCommand = ''(${pkgs.git}/bin/git ls-tree -r --name-only HEAD || ${pkgs.silver-searcher}/bin/ag --hidden --ignore .git -g "")'';
   };
 }
--- modules/home/software/karabiner/default.nix
+++ modules/home/software/karabiner/default.nix
@@ -7,7 +7,13 @@ let
 in {
   options.shabka.karabiner.enable = mkEnableOption "Enable karabiner (only relevant for Darwin hosts).";
 
-  config = mkIf (cfg.enable && (config.shabka.darwinConfig != {})) {
+  config = mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = config.shabka.darwinConfig != {};
+        message = "Karabiner isn't used on Linux hosts. It should thus be disabled on them.";
+      }
+    ];
     home.file.".config/karabiner/karabiner.json".source = ./karabiner.json;
   };
 }
