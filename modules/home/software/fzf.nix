3b2b88cd87921ace9dc16172e373e1498244b405
risson <18313093+rissson@users.noreply.github.com>
2020-01-23 16:40:01 +0100

    shabka: gate several options (#303)
    * shabka.fzf.enable
    
    * shabka.karabiner.enable
    
    * shabka.fzf.enable fix
    
    * apply requested changes

--- modules/home/software/fzf.nix
+++ modules/home/software/fzf.nix
@@ -1,8 +1,13 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
-{
-  # enable FZF
-  programs.fzf = {
+with lib;
+
+let
+  cfg = config.shabka.fzf;
+in {
+  options.shabka.fzf.enable = mkEnableOption "Enable fzf - a command-line fuzzy finder.";
+
+  config.programs.fzf = mkIf cfg.enable {
     enable = true;
     defaultCommand = ''(${pkgs.git}/bin/git ls-tree -r --name-only HEAD || ${pkgs.silver-searcher}/bin/ag --hidden --ignore .git -g "")'';
   };
--- modules/home/software/karabiner/default.nix
+++ modules/home/software/karabiner/default.nix
@@ -2,8 +2,18 @@
 
 with lib;
 
-{
-  config = mkIf (config.shabka.darwinConfig != {}) {
+let
+  cfg = config.shabka.karabiner;
+in {
+  options.shabka.karabiner.enable = mkEnableOption "Enable karabiner (only relevant for Darwin hosts).";
+
+  config = mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = config.shabka.darwinConfig != {};
+        message = "Karabiner isn't used on Linux hosts. It should thus be disabled on them.";
+      }
+    ];
     home.file.".config/karabiner/karabiner.json".source = ./karabiner.json;
   };
 }
