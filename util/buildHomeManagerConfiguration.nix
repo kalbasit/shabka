56bce1e1f68be0cb2fc1151aba16484d9b553986
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-02-24 17:44:14 +0100

    adapt cli to be used with non-nixos hosts

--- util/buildHomeManagerConfiguration.nix
+++ util/buildHomeManagerConfiguration.nix
@@ -1,9 +1,10 @@
 { pkgs, lib }:
 
 let
-  external = import <shabka/external> {};
+  shabka = import <shabka> {};
+  release = builtins.getEnv "RELEASE";
 in {
-  buildHomeManagerConfiguration = conf: (import "${external.home-manager.unstable.path}/home-manager/home-manager.nix" {
+  buildHomeManagerConfiguration = conf: (import "${shabka.external.home-manager."${release}".path}/home-manager/home-manager.nix" {
     confPath = conf;
     confAttr = "";
   }).activationPackage;
