64d2495954ba3e3d09293ad3bb1c7bafaeb2dd53
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-04-18 22:01:28 +0200

    external: home-manager: add 20.03 release

---
+++ external/home-manager/20.03/default.nix
@@ -0,0 +1,20 @@
+{ mkExternal }:
+
+let
+  pinnedVersion = builtins.fromJSON (builtins.readFile ./version.json);
+
+  src = builtins.fetchTarball {
+    inherit (pinnedVersion) url sha256;
+  };
+
+  patches = [];
+
+  patched = mkExternal {
+    inherit src patches;
+
+    name = "home-manager-release-20-03";
+    revision = pinnedVersion.rev;
+  };
+in {
+  path = patched;
+}
