0cc6dbff63dca777d376f5dc3eb24d6d5c369e5f
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2020-04-18 23:57:58 +0200

    external: nixpkgs: 20.03: add virtualbox patches

--- external/nixpkgs/20.03/default.nix
+++ external/nixpkgs/20.03/default.nix
@@ -11,6 +11,9 @@ let
     ../19.09/0002-flashplayer-404
     # https://github.com/NixOS/nixpkgs/pull/85373
     ./0001-flashplayer-404.patch
+    # https://github.com/NixOS/nixpkgs/pull/82693
+    ./0002-virtualbox.patch
+    ./0003-virtualbox.patch
   ];
 
   patched = mkExternal {
