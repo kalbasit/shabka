764bd38b302a13f0eff6dc70024297d47a98a11d
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2020-08-04 20:21:24 -0700

    overlays/swm: 0.4.0-alpha1 -> 0.4.0-alpha2

--- overlays/swm.nix
+++ overlays/swm.nix
@@ -1,11 +1,11 @@
 self: super:
 
 let
-  version = "0.4.0-alpha1";
+  version = "0.4.0-alpha2";
 
   swm-src = builtins.fetchTarball {
     url = "https://github.com/kalbasit/swm/archive/v${version}.tar.gz";
-    sha256 = "17s6ccwg4240b6ykam27pz4kisyfm977rh1zzzx6ykarqi0x2fkf";
+    sha256 = "0iwgccsxgx1fxv90vvwz73bmh65ik7i6qc7hmw1rwhh7830m0agp";
   };
 in {
   swm = super.shabka.external.nixpkgs.release-unstable.callPackage "${swm-src}/default.nix" { inherit version; };
