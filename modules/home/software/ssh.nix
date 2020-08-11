5289b3e34bc350fa499f34874f860d74e26f2897
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2020-08-10 17:28:47 -0700

    home/ssh: remove privateSSHPath

--- modules/home/software/ssh.nix
+++ modules/home/software/ssh.nix
@@ -7,30 +7,25 @@ let
   cfg = config.shabka.ssh;
 
 in {
+  imports = [
+    (mkRemovedOptionModule ["shaka" "ssh" "privateSSHPath"] "Set programs.ssh directly")
+  ];
+
   options.shabka.ssh = {
     enable = mkEnableOption "SSH configurations";
-
-    privateSSHPath = mkOption {
-      type = types.nullOr types.path;
-      default = null;
-      defaultText = ''
-        The path to the private SSH module
-      '';
-    };
   };
 
   config = mkIf cfg.enable {
+    programs.ssh = {
+      enable = true;
 
-    programs.ssh = if (cfg.privateSSHPath != null) then import cfg.privateSSHPath else {
-        enable = true;
-
-        extraConfig = ''
-          KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
-          PubkeyAuthentication yes
-          HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa
-          Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
-          MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
-        '';
+      extraConfig = ''
+        KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
+        PubkeyAuthentication yes
+        HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa
+        Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+        MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
+      '';
     };
   };
 }
